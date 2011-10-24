5c5991511f41c41675eb72e90d56d07cfe5670fe
Simon Davie <nexx@nexxdesign.co.uk>
2011-10-24 02:12:18 +0100

    BatteryMeterDrawable: add hidden option (1/2)
     * Remove the tuner call for icon blacklisting
       since it is no longer needed
    
    Change-Id: Icabe7f86e5d46f5d1ae70dbf250a64535411883d
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7958,6 +7958,7 @@ public final class Settings {
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
          * 2: Display the battery as text
+         * 3: Do not display the battery
          * default: 0
          * @hide
          */
@@ -7965,7 +7966,7 @@ public final class Settings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 3);
 
         /**
          * This are the settings to be backed up.
