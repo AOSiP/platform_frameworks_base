5c7035f92a3a58d91f6fa65de82000a5257589be
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:17:27 +0000

    Adb over Network: add QS tile [1/3]
    abc ezio84: remove the settings part, never used it because
    the tile is handy
    adapt code to O notification builder
    unlink from commits to hide adb notification
    
    ----
    
    Includes:
    - ADB Over Network, integration of the adb-host mode (already present)
      The feature can be used/tested without the Settings part:
    
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
      Author: sssemil <suleymanovemil8@gmail.com>
      Show notification on adb over network too
    
      Screenshots - http://goo.gl/TgsRI6
      Id: I9ddc0aa9a4f330a06ab5d97a8645d1b31bb6f299
    
      Author: Ethan Chen <intervigil@gmail.com>
      Use custom ADB over wifi property
      * Use a custom system property to trigger the real one, so we avoid
      running afoul of any SELinux CTS requirements.
    
    Change-Id: Ie9e8108af18937abec046f66cdf56794b1bb6d90
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7684,6 +7684,12 @@ public final class Settings {
         public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
 
         /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
