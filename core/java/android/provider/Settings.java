71fddd826aea8c9a4e35c1ef37aaf29b1c0eb84c
rascarlo <rascarlo@gmail.com>
2014-11-12 14:34:17 +0100

    volume rocker: volume key cursor control (1/2)
    Change-Id: I35713c7d7efdadfdb5f4f2461a7f90ecde6805dc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3832,6 +3832,15 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
 
         /**
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
