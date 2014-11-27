196937395873d63417c350ace8d1131143d4a4c2
Danesh M <daneshm90@gmail.com>
2014-11-27 14:17:19 +0100

    BatteryMeterDrawable: add text style (1/2)
    Change-Id: I4820b2bea7c6f38b6a9e1b81a6e115ae1101fac9
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7957,6 +7957,7 @@ public final class Settings {
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
          * 1: Display the battery as a circle
+         * 2: Display the battery as text
          * default: 0
          * @hide
          */
@@ -7964,7 +7965,7 @@ public final class Settings {
 
         /** @hide */
         public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
         /**
          * This are the settings to be backed up.
