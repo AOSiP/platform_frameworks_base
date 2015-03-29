89276b4f0f42d01fecf89b0267e113708343c6f6
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    base: allow disable of screenshot shutter sound [1/2]
    fox: disable by default
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4424,6 +4424,13 @@ public final class Settings {
         /** @hide */
         private static final Validator USE_OLD_MOBILETYPE_VALIDATOR = BOOLEAN_VALIDATOR;
 
+         /**
+          * Screenshod sound enable, This is the noise made when taking a screesnhot
+          * Defaults to 1 - sounds enabled
+          * @hide
+          */
+         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
