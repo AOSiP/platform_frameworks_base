f51d68f05b67864c5d84e9e1bb547c6d85259697
nadlabak <pavel@doshaska.net>
2015-06-07 02:01:05 +0200

    PowerManagerService: Fix updating of mUserActivitySummary
    I7f1fc35a1573717d1ea101a07c4171d6f66d1553 missed the fact that the primary
    purpose of the affected condition block was to update mUserActivitySummary
    and the button/keyboard light handling was just appended to it later.
    
    This fixes the waking from dream/screensaver by user activity.
    
    I30c5c8c9c09e3d57ace18cac72b783510b9b3bf3 is removed here as well as it was
    just a band aid.
    
    jira: NIGHTLIES-1285
    
    Change-Id: I6b2f6c58e73110787d62e86d4d2ef538638cf491

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1838,7 +1838,7 @@ public final class PowerManagerService extends SystemService
                 final boolean userInactiveOverride = mUserInactiveOverrideFromWindowManager;
 
                 mUserActivitySummary = 0;
-                if (mWakefulness == WAKEFULNESS_AWAKE && mLastUserActivityTime >= mLastWakeTime) {
+                if (mLastUserActivityTime >= mLastWakeTime) {
                     nextTimeout = mLastUserActivityTime
                             + screenOffTimeout - screenDimDuration;
                     if (now < nextTimeout) {
