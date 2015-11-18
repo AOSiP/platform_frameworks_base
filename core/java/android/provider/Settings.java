af4409d02829c37eb38f257c47b14227afdf67cc
maxwen <max.weninger@gmail.com>
2015-11-18 21:43:53 +0100

    [1/2] base: unlink notification volume is back
    -delete old unused volume images
    -add new one for notification volumes
    maybe some day one will be able to create a vector
    drawable for this - I am not :)
    
    base: add vector for notification volume mute
    
    thanks to Marko Man
    
    Change-Id: I85f63ecb5b8c4cc359c8ffeb625a582c67354917

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4237,6 +4237,13 @@ public final class Settings {
         public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
