d77dc32729f9e062348102bd2377887a93559903
eyosen <abittin@gmail.com>
2015-12-05 18:36:19 +0100

    Add Screen Record to the Power Menu [1/2]
    Change-Id: I1ea476c43db6b45031b04705319cbdef2d9be24d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3974,6 +3974,13 @@ public final class Settings {
         public static final String POWERMENU_TORCH = "powermenu_torch";
 
         /**
+         * Whether to display the screenrecord option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
+
+        /**
          * Double tap on navbar to sleep
          * @hide
          */
