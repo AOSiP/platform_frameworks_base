22f8dbd0f9e286d6d71a4319bc595ee0aba331e9
BigBrother1984 <carlosavignano@aospa.co>
2016-03-26 16:55:22 -0400

    Improve Heads up toggle [1/2]
    * Add multiuser support, provide a system setting to override heads up
      showing. User can disable heads up within this one so we don't need to
      mess with Settings.Global heads up dependencies and break Google wide
      implementations like priority filtering etc.
    *updated heads up quicksetting tile to reflect changes as well
    
    PureNexus Edits:
    *Adapted for Marshmallow/Nougat
    
    Change-Id: I09aeba75d7d0f0b90ba58d6f99b4bcf7395624c6

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HeadsUpTile.java
@@ -20,11 +20,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
-import android.provider.Settings.Global;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
-import com.android.systemui.qs.GlobalSetting;
+
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.R;
 
@@ -33,17 +32,8 @@ public class HeadsUpTile extends QSTile<QSTile.BooleanState> {
 
     private static final Intent SOUND_SETTINGS = new Intent("android.settings.SOUND_SETTINGS");
 
-    private final GlobalSetting mSetting;
-
     public HeadsUpTile(Host host) {
         super(host);
-
-        mSetting = new GlobalSetting(mContext, mHandler, Global.HEADS_UP_NOTIFICATIONS_ENABLED) {
-            @Override
-            protected void handleValueChanged(int value) {
-                handleRefreshState(value);
-            }
-        };
     }
 
     @Override
@@ -52,8 +42,8 @@ public class HeadsUpTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
-    protected void handleClick() {
-        setEnabled(!mState.value);
+    public void handleClick() {
+        setEnabled();
         refreshState();
     }
 
@@ -68,19 +58,23 @@ public class HeadsUpTile extends QSTile<QSTile.BooleanState> {
         return mContext.getString(R.string.quick_settings_heads_up_label);
     }
 
-    private void setEnabled(boolean enabled) {
-        Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED,
-                enabled ? 1 : 0);
+    private boolean getUserHeadsUpState() {
+         return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON) != 0;
+    }
+
+    private void setEnabled() {
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                getUserHeadsUpState() ? 0 : 1);
     }
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
-        final boolean headsUp = value != 0;
-        state.value = headsUp;
+        state.value = getUserHeadsUpState();
         state.label = mContext.getString(R.string.quick_settings_heads_up_label);
-        if (headsUp) {
+        if (getUserHeadsUpState()) {
             state.icon = ResourceIcon.get(R.drawable.ic_qs_heads_up_on);
             state.contentDescription =  mContext.getString(
                     R.string.accessibility_quick_settings_heads_up_on);
