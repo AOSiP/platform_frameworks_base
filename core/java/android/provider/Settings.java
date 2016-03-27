c7431ed2ba35385cb190547f08977917a886202e
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4333,6 +4333,13 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
