bad513fc828d03e5d99ec571205367832689c014
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to QS tiles [1/2]
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5204,6 +5204,20 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
