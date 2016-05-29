a35616984d2cc507f19a1667602284dc9510523f
Nico60 <ndhouailly@gmail.com>
2016-05-29 13:05:37 +0200

    Add interpolators to qs tiles animation [1/2]
    Change-Id: I9a34baeccc25a69abf5b9a1cddcd92c058a735d3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3636,12 +3636,19 @@ public final class Settings {
          */
         public static final String ANIM_TILE_STYLE = "anim_tile_style";
 
-		/**
+        /**
          * Change quick settings tiles animation duration
          *
          * @hide
          */
         public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
+         * Change quick settings tiles interpolator
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
