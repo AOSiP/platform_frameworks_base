1c30b81b7f158976778fb15ef0fb3d62fda745e7
Alex <mazdarider23@gmail.com>
2016-09-07 03:22:13 -0400

    Flashlight notification toggle [1/2]
    - Off by default
    
    Change-Id: Ic40da4aa7a67818725c971c1ee018fc8b7bd4535

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3880,6 +3880,12 @@ public final class Settings {
         public static final String DISABLE_DASHBOARD_CONDITIONS = "disable_dashboard_conditions";
 
         /**
+         *  Enable flashlight notification
+         * @hide
+         */
+        public static final String FLASHLIGHT_NOTIFICATION = "flashlight_notification";
+
+        /**
          * Whether to show statusbar clock in the keyguard
          * @hide
          */
