12e09201451901225b62471b735c5608f87f5745
Alex <mazdarider23@gmail.com>
2016-09-09 01:09:13 -0400

    Immersive Recents [1/2]
    Name says it all. Made this when someone had asked about how recents look with Touchwiz, simple enough.
    
    May expand this to other shit but for now, it do what it do hahaha
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Change-Id: I3a757bf833a3ea5285d7e74fe14cdfe4d78ff437

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3878,6 +3878,16 @@ public final class Settings {
         public static final String SCREENRECORD_CHORD_TYPE = "screenrecord_chord_type";
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Show or hide clock
          * 0 - hide
          * 1 - show (default)
