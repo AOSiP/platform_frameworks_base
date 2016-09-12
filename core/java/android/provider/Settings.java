1c8211441c44be9390c5e3312cddf30fe5752d0f
Alex <mazdarider23@gmail.com>
2016-09-12 17:55:33 -0400

    Disable dashboard suggestions [2/2]
    Change-Id: I33879e9b8e1ef15898e781aad4766b755df36287

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3611,6 +3611,12 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
