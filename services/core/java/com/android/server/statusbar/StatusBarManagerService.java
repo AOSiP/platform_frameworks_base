30ca44d5ac0f2ff065d3ba90e6ceb52e180c070d
Paul Henschel <drcmda@gmail.com>
2016-09-25 16:03:30 +0200

    frameworks: PIE 3.0 (nougat)
    Pie is an alternative navigation and system info interface. It replaces the
    standard navbar/statusbar with a navigation arc decorated with statusbar info.
    
    This also contains full theming support.
    
    Key contributors:
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Jesus David <jesdga95@gmail.com>
    Signed-off-by: Curtis Mayers <curtis.mayers@gmail.com>
    Signed-off-by: David L.-W. <david.leppla.weber@gmail.com>
    Signed-off-by: Paul Henschel <drcmda@gmail.com>
    Signed-off-by: Aaron Gascoigne <aarongascoigne@aospa.co>
    Signed-off-by: Alexis Rico <sferadev@gmail.com>
    Signed-off-by: Anas Karbila <anaskarbila@aospa.co>
    Signed-off-by: Oliver Reiche <oliverreiche@aospa.co>
    
    Redesign done by:
    Signed-off-by: Anas Karbila <anaskarbila@aospa.co>
    Signed-off-by: Oliver Reiche <oliverreiche@aospa.co>
    
    Ticket: NOUGAT-11
    
    Ported to RR by @varund7726
    
    Change-Id: I99ec161c48c2a871e140b943bdcd7847f594fffe
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -763,6 +763,17 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
         }
     }
 
+    @Override
+    public void toggleOrientationListener(boolean enable) {
+        if (mBar != null) {
+            try {
+                mBar.toggleOrientationListener(enable);
+            } catch (RemoteException ex) {
+                // system is dead
+            }
+        }
+    }
+
     private void enforceStatusBar() {
         mContext.enforceCallingOrSelfPermission(android.Manifest.permission.STATUS_BAR,
                 "StatusBarManagerService");
