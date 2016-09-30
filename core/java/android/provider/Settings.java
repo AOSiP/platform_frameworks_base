54f1144f827170de6a2fbf0729ed205550f0e5d1
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-09-30 07:22:19 -0400

    SystemUI: Visualizer customizations
    mydongistiny: Fixed up for pie and cleaned up Visualizer
    aosip: adapt to ambient visualizer changes
    Change-Id: Ife08e2de82ce1598b3541b18721308d75ed148aa
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5118,6 +5118,18 @@ public final class Settings {
         public static final Validator PROXIMITY_ON_WAKE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         *  Weather to use a custom color for the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_USE_CUSTOM_COLOR = "lock_screen_visualizer_use_custom_color";
+
+        /**
+         *  Custom color of the visualizer on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
