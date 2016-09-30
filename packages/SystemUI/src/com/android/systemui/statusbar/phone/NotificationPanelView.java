71047408346049233e4ad7e8d6cf42d5b9b2c4dd
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-09-30 07:22:19 -0400

    SystemUI: Visualizer customizations
    mydongistiny: Fixed up for pie and cleaned up Visualizer
    aosip: adapt to ambient visualizer changes
    Change-Id: Ife08e2de82ce1598b3541b18721308d75ed148aa
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1851,6 +1851,7 @@ public class NotificationPanelView extends PanelView implements
                 * mKeyguardStatusBarAnimateAlpha);
         mKeyguardStatusBar.setVisibility(mKeyguardStatusBar.getAlpha() != 0f
                 && !mDozing ? VISIBLE : INVISIBLE);
+        mStatusBar.getVisualizer().setAlpha(mKeyguardStatusBar.getAlpha());
     }
 
     private void updateKeyguardBottomAreaAlpha() {
