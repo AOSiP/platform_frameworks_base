a354dc2476ec3babcf15a25e2006bb9183f95c57
beanstown106 <nbenis106@gmail.com>
2016-10-29 02:14:58 -0400

    Keyguard: Add ability to disable lockscreen ripple [1/2]
    BenzoEdit: Moved from Keyguard to SystemUI
    Change-Id: Ic690be70292ac3d079640adaffd7601d7943b981
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4548,6 +4548,12 @@ public final class Settings {
                 "status_bar_ticker_animation_mode";
 
         /**
+         * Whether to disable ripple on lockscreen pin entry
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_RIPPLE = "lockscreen_pin_ripple";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
