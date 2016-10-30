343c5e9350bfe43dfa34c4c9a42a68d17c9e7500
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-30 20:04:25 +0100

    Keyguard: Show charging current & voltage on lockscreen [2/2]
    Thanx to ezio84 for the switch ;)
    
    Change-Id: I610e714f09172f0512adc1c12e594c2e4c340280
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4266,6 +4266,12 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+
         /**
          * Stores the java DateFormat string for the date
          * @hide
