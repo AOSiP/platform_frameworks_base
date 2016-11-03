304aa1cb25a97e953bdb2b90eb09ff7ba9010c59
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-11-04 00:39:34 +0100

    [2/2]Base: show more battery info on lockscreen when charging
    - negociated max. charging current
    - negociated max. charging voltage
    - current battery temperature
    
    Change-Id: I802d85beec1391e27ec2e0b9c0dd9f45da85864d
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3779,6 +3779,12 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * Dashboard portrait columns in settings
          * @hide
          */
