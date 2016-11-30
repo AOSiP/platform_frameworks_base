f143933f01c141e154dccd417ad2aca3f9c0588f
Joe Maples <joe@frap129.org>
2016-11-30 21:13:15 +0000

    QS Panel: Add the ability to hide/show Edit icon [1/2]
    aex edits(hamza-badar):
      * Adapt for oreo
      * Cleanup
    
    Adds toggle for the quick tiles edit
    
    nathanchance edit: Switched the logic so that enabling the setting disables the icon so that all switches are disabled by default. Also removed the Settings expanded option.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    
    Change-Id: I496ea08eed21eea9e852d0f4760d615d2067f9e4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5084,6 +5084,12 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Whether to show or hide the edit icon
+         * @hide
+         */
+        public static final String QS_EDIT_TOGGLE = "qs_edit_toggle";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
