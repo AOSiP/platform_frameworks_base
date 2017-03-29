64743dba92c5ad48a71cd78c4a5de0ec6618699c
Carlo Savignano <carlosavignano@aospa.co>
2017-03-29 15:09:00 +0100

    Add gestures control [3/4]
    * Database policy.
    * KeyHandler for gestures inputs.
    * Input policy integration.
    * Supported ALL QCOM chipsets synaptic gestures.
    
    Ticket: NOUGAT-17
    
    Change-Id: I666cfae68c1e383d2f8a842204788c3e23cd2209
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4049,6 +4049,84 @@ public final class Settings {
 
 
         /**
+         * Controls whether gestures are enabled.
+         * @hide
+         */
+        public static final String GESTURES_ENABLED = "gestures_enabled";
+
+        /**
+         * Controls whether draw V gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_V = "gesture_draw_v";
+
+        /**
+         * Controls whether draw inverse V gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_INVERSE_V = "gesture_draw_inverse_v";
+
+        /**
+         * Controls whether draw O gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_O = "gesture_draw_o";
+
+        /**
+         * Controls whether draw M gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_M = "gesture_draw_m";
+
+        /**
+         * Controls whether draw W gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_W = "gesture_draw_w";
+
+        /**
+         * Controls whether draw arrow left gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_ARROW_LEFT = "gesture_draw_arrow_left";
+
+        /**
+         * Controls whether draw arrow right gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_DRAW_ARROW_RIGHT = "gesture_draw_arrow_right";
+
+        /**
+         * Controls whether one finger swipe up gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_ONE_FINGER_SWIPE_UP = "gesture_one_finger_swipe_up";
+
+        /**
+         * Controls whether one finger swipe right gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_ONE_FINGER_SWIPE_RIGHT = "gesture_one_finger_swipe_right";
+
+        /**
+         * Controls whether one finger swipe down gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_ONE_FINGER_SWIPE_DOWN = "gesture_one_finger_swipe_down";
+
+        /**
+         * Controls whether one finger swipe left gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_ONE_FINGER_SWIPE_LEFT = "gesture_one_finger_swipe_left";
+
+        /**
+         * Controls whether two finger swipe gesture is enabled.
+         * @hide
+         */
+        public static final String GESTURE_TWO_FINGER_SWIPE = "gesture_two_finger_swipe";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4104,7 +4182,19 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             ACCELEROMETER_ROTATION,
-            SHOW_BATTERY_PERCENT
+            SHOW_BATTERY_PERCENT,
+            GESTURES_ENABLED,
+            GESTURE_DRAW_V,
+            GESTURE_DRAW_INVERSE_V,
+            GESTURE_DRAW_O,
+            GESTURE_DRAW_M,
+            GESTURE_DRAW_W,
+            GESTURE_DRAW_ARROW_LEFT,
+            GESTURE_DRAW_ARROW_RIGHT,
+            GESTURE_ONE_FINGER_SWIPE_UP,
+            GESTURE_ONE_FINGER_SWIPE_RIGHT,
+            GESTURE_ONE_FINGER_SWIPE_DOWN,
+            GESTURE_ONE_FINGER_SWIPE_LEFT,
         };
 
         /**
@@ -7195,8 +7285,8 @@ public final class Settings {
             MOUNT_UMS_AUTOSTART,
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
-            SLEEP_TIMEOUT,
             DOUBLE_TAP_TO_WAKE,
+            SLEEP_TIMEOUT,
             WAKE_GESTURE_ENABLED,
             LONG_PRESS_TIMEOUT,
             CAMERA_GESTURE_DISABLED,
