de06bb1cbe831a693125c37545956d032604ae76
ezio84 <brabus84@gmail.com>
2017-04-04 10:18:51 +0200

    SlimRecents: refresh sizes on dpi change [2/2]
    Change-Id: I5c0b6da8cd3d29e63ba1fc0a6d6e33879bf1ac0a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4267,6 +4267,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
         mNetworkController.onConfigurationChanged();
+        if (mSlimRecents != null) {
+            mSlimRecents.onConfigurationChanged(newConfig);
+        }
     }
 
     @Override
