d1dad913116a220a687ff1b0ea0f7575c54eb083
ezio84 <brabus84@gmail.com>
2017-05-11 12:43:29 +0200

    Launch music player on headset connect: add BT compatibility [1/2]
    allow also to exclude carkits, the user might want to start
    android auto or other car audio apps instead.
    
    also move the setting check into the existing SettingsObserver
    
    AOSiP Edits:
    Update for Pie, fixup int array check logic
    
    Change-Id: I2d91c0716b72f55c1161cb9c281206a737846053
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5134,6 +5134,12 @@ public final class Settings {
 
         /**
          * Whether to launch default music player when headset plugged in
+         * 0 = don't do anything (default)
+         * 1 = launch only on wired connection
+         * 2 = launch only on bt connection but no carkit
+         * 3 = launch only on bt connection
+         * 4 = launch on both connection types but no carkit
+         * 5 = launch on both connection types
          * @hide
          */
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
@@ -5629,7 +5635,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DOZE_ON_CHARGE);
-
+            PRIVATE_SETTINGS.add(HEADSET_CONNECT_PLAYER);
             // Pocket mode handler.
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
         }
