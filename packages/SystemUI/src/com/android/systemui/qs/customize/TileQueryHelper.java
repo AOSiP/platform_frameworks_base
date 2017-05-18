4d1bada14ce32fc458e784a1e8e4381c1dd0bdf6
xlxfoxxlx <joshfox87@gmail.com>
2017-05-17 22:22:27 -0400

    TileQueryHelper: Use quick_settings_tiles_stock for qs tile list
    Change-Id: I04bcc5e1aac0bb73d72b049f822fe9e76f0fc447

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -58,8 +58,7 @@ public class TileQueryHelper {
     }
 
     private void addSystemTiles(final QSTileHost host) {
-        String possible = mContext.getString(R.string.quick_settings_tiles_default)
-                + ",hotspot,inversion,saver,work,cast,night,caffeine,heads_up";
+        String possible = mContext.getString(R.string.quick_settings_tiles_stock);
         String[] possibleTiles = possible.split(",");
         final Handler qsHandler = new Handler(host.getLooper());
         final Handler mainHandler = new Handler(Looper.getMainLooper());
