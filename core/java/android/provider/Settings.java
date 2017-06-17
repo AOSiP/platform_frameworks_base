5afdd49d6a78de5e5a5b7431fc88de5ca601e4a6
Danny Baumann <dannybaumann@web.de>
2017-06-17 19:45:41 +0200

    Allow home button to wake device [1/2]
    Change-Id: I20aa4c5ba96ab9da625c3957f604b07284f5b9e6
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4320,13 +4320,10 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
-         * Wheter to play notification sound and vibration if screen is ON
-         * 0 - never
-         * 1 - always
-         * 2 - if media playing
+         * Whether to allow waking up screen on taping home button
          * @hide
          */
-        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+        public static final String HOME_WAKE_SCREEN = "home_wake_screen";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
