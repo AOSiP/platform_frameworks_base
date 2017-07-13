6d6a7eb4a4810880166a4aa936f8bcf081404538
xlxfoxxlx <joshfox87@gmail.com>
2017-07-12 23:00:39 -0400

    Revert "[1/2] base: featureize ro.recents.grid"
    This reverts commit 2d34b9e2ea07c8e32f90ee618816e560f39b3e32.
    
    Change-Id: I8ac50bc691502322d38fef46fc744189eb72aa68

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -759,9 +759,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mScreenrecordChordVolumeUpKeyConsumed;
     private boolean mScreenrecordChordType;
 
-    // AOSP grid style recents
-    private boolean mAospRecentsGrid;
-
     /* The number of steps between min and max brightness */
     private static final int BRIGHTNESS_STEPS = 10;
 
@@ -2434,8 +2431,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.VOLUME_WAKE_SCREEN, 0, UserHandle.USER_CURRENT) == 1);
             mScreenrecordChordType = (Settings.System.getIntForUser(resolver,
                     Settings.System.SCREENRECORD_CHORD_TYPE, 0, UserHandle.USER_CURRENT) == 1);
-            mAospRecentsGrid = (Settings.System.getIntForUser(resolver,
-                    Settings.System.RECENTS_USE_GRID, 0, UserHandle.USER_CURRENT) == 1);
             mTorchEnabled = (Settings.System.getIntForUser(resolver,
                     Settings.System.KEYGUARD_TOGGLE_TORCH, 0, UserHandle.USER_CURRENT) == 1);
 
