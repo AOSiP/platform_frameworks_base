8c5daccd6e76caac05fa4ca95d430f4548762d5d
ezio84 <brabus84@gmail.com>
2017-07-27 17:54:33 +0200

    Allow to disable battery light when in DND mode [1/2]
    the battery led can be annoying during night or when you play
    a game with a connected charger.
    
    Change-Id: I70e9ee1261b97c7b4baf4506cb4098ea550a27f6

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -166,6 +166,10 @@ public final class BatteryService extends SystemService {
     private int mBatteryReallyFullARGB;
     private boolean mMultiColorLed;
 
+    //Battery light on DND
+    private boolean mAllowBatteryLightOnDnd;
+    private boolean mIsDndActive;
+
     private boolean mSentLowBatteryBroadcast = false;
 
     private final int mVbattSamplingIntervalMsec = 30000; /* sampling frequency - 30 seconds */
@@ -982,7 +986,7 @@ public final class BatteryService extends SystemService {
 
             final int level = mBatteryProps.batteryLevel;
             final int status = mBatteryProps.batteryStatus;
-            if (!mLightEnabled) {
+            if (!mLightEnabled || (mIsDndActive && !mAllowBatteryLightOnDnd)) {
                 // No lights if explicitly disabled
                 mBatteryLight.turnOff();
             } else if (level < mLowBatteryWarningLevel) {
@@ -1117,6 +1121,12 @@ public final class BatteryService extends SystemService {
                 resolver.registerContentObserver(
                         Settings.System.getUriFor(Settings.System.BATTERY_LIGHT_REALLY_FULL_COLOR),
                         false, this, UserHandle.USER_ALL);
+                resolver.registerContentObserver(
+                        Settings.System.getUriFor(Settings.System.BATTERY_LIGHT_ALLOW_ON_DND),
+                        false, this, UserHandle.USER_ALL);
+                resolver.registerContentObserver(
+                        Settings.Global.getUriFor(Settings.Global.ZEN_MODE),
+                        false, this, UserHandle.USER_ALL);
             }
 
             update();
@@ -1137,6 +1147,13 @@ public final class BatteryService extends SystemService {
             // Low battery pulse
             mLedPulseEnabled = Settings.System.getInt(resolver,
                         Settings.System.BATTERY_LIGHT_PULSE, 0) != 0;
+
+            // DND battery light
+            mAllowBatteryLightOnDnd = Settings.System.getInt(resolver,
+                    Settings.System.BATTERY_LIGHT_ALLOW_ON_DND, 1) == 1;
+            mIsDndActive = Settings.Global.getInt(resolver,
+                    Settings.Global.ZEN_MODE, Settings.Global.ZEN_MODE_OFF)
+                    != Settings.Global.ZEN_MODE_OFF;
 
             // Light colors
             mBatteryLowARGB = Settings.System.getInt(resolver,
