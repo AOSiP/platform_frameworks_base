7f669c53cfaea24110bb79fc0de86c27070afdca
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    This change deprecates ThemeInterfacer, so for testers, please do not
    compile ThemeInterfacer with this change!
    
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8041,6 +8041,13 @@ public final class Settings {
         public static final String QS_AUTO_ADDED_TILES = "qs_auto_tiles";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * Enable face auto unlock on secure lock screens
          *
          * @hide
