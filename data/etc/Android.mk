bb8ea820c7ce998f73cacbf53bf49e80d784fa7b
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    This change deprecates ThemeInterfacer, so for testers, please do not
    compile ThemeInterfacer with this change!
    
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

--- data/etc/Android.mk
+++ data/etc/Android.mk
@@ -42,6 +42,15 @@ include $(BUILD_PREBUILT)
 
 ########################
 include $(CLEAR_VARS)
+LOCAL_MODULE := projekt.substratum.theme.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+########################
+include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-google.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
