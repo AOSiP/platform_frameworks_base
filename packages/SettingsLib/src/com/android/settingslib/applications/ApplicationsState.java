1ec9e066e54522ef16dc028336f7f31ddb29a225
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -1632,7 +1632,6 @@ public class ApplicationsState {
             return false;
         }
     };
-
     public static final AppFilter FILTER_DISABLED = new AppFilter() {
         @Override
         public void init() {
