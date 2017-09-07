5fa0814f777a9262ff97cf4f36d5e0a211bf200c
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    base: Disable dashboard suggestions and conditions [1/3]
    Change-Id: I47bdefccd70f4013e4ea7ced7e125bebdb510fc3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5655,6 +5655,18 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether to show dashboard conditions in Settings
+         * @hide
+         */
+        public static final String SETTINGS_SHOW_CONDITIONS = "settings_show_conditions";
+
+        /**
+         * Whether to show dashboard suggestions in Settings
+         * @hide
+         */
+        public static final String SETTINGS_SHOW_SUGGESTIONS = "settings_show_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
