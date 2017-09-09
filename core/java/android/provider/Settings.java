7e063b386d6eb09025bf7202900bf2f16fbfc07d
Konsta <konsta09@gmail.com>
2017-09-09 19:10:41 +0800

    Framework: Volume key cursor control (1/2) This feature is moved to framework so it also works with third party keyboards.
    Change-Id: I4993d655dc1fde1b465df6af1d196765086a3daf
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4146,6 +4146,15 @@ public final class Settings {
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Indicates whether ANBI (Accidental navigation button interaction) is enabled.
          *
          * @hide
