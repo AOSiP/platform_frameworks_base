357104dc6f8b21fc906fb6ab9b857102da07b17a
kameron19921016 <kameron19921016@yahoo.com>
2017-09-15 09:13:30 +0800

    FWB: Add Volume Key Answer (1/2)
    Change-Id: Icd70db06699da3c81d7c596b4305a8384774c055
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4378,6 +4378,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * volume answer
+         * @hide
+         */
+        public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
