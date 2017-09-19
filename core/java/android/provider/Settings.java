0cd0d7d3f36f98820d3342be06b2c285bce019ee
Alex Cruz <mazdarider23@gmail.com>
2017-09-19 19:15:26 +0800

    Pimp the power menu out [1/2]
    xyyx
    -reworked for P
    -added toggle for users
    -added toggle for logout
    -lock, settings icons replaced with Android Studio vectors
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4675,6 +4675,115 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to display sound panel in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SOUNDPANEL = "powermenu_soundpanel";
+        /** @hide */
+        private static final Validator POWERMENU_SOUNDPANEL_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display screenshot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENSHOT = "powermenu_screenshot";
+        /** @hide */
+        private static final Validator POWERMENU_SCREENSHOT_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display settings in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SETTINGS = "powermenu_settings";
+        /** @hide */
+        private static final Validator POWERMENU_SETTINGS_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display lock in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_LOCKDOWN = "powermenu_lockdown";
+        /** @hide */
+        private static final Validator POWERMENU_LOCKDOWN_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display airplane in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_AIRPLANE = "powermenu_airplane";
+        /** @hide */
+        private static final Validator POWERMENU_AIRPLANE_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display reboot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_RESTART = "powermenu_restart";
+        /** @hide */
+        private static final Validator POWERMENU_RESTART_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display advanced reboot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_RESTART_RECOVERY = "powermenu_restart_recovery";
+        /** @hide */
+        private static final Validator POWERMENU_RESTART_RECOVERY_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display power in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_POWER = "powermenu_power";
+        /** @hide */
+        private static final Validator POWERMENU_POWER_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display the users option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_USERS = "powermenu_users";
+        /** @hide */
+        private static final Validator POWERMENU_USERS_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display the users option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_LOGOUT = "powermenu_logout";
+        /** @hide */
+        private static final Validator POWERMENU_LOGOUT_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to display the power menu dialog while on the lockscreen
+         * @hide
+         */
+        public static final String POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
+        /** @hide */
+        private static final Validator POWERMENU_LOCKSCREEN_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4756,6 +4865,17 @@ public final class Settings {
             BATTERY_LIGHT_FULL_COLOR,
             BATTERY_LIGHT_REALLYFULL_COLOR,
             FINGERPRINT_SUCCESS_VIB,
+            POWERMENU_SOUNDPANEL,
+            POWERMENU_SCREENSHOT,
+            POWERMENU_SETTINGS,
+            POWERMENU_LOCKDOWN,
+            POWERMENU_AIRPLANE,
+            POWERMENU_RESTART,
+            POWERMENU_RESTART_RECOVERY,
+            POWERMENU_POWER,
+            POWERMENU_USERS,
+            POWERMENU_LOGOUT,
+            POWERMENU_LOCKSCREEN,
         };
 
         /**
@@ -4895,6 +5015,17 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_FULL_COLOR);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_REALLYFULL_COLOR);
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
+            PRIVATE_SETTINGS.add(POWERMENU_SOUNDPANEL);
+            PRIVATE_SETTINGS.add(POWERMENU_SCREENSHOT);
+            PRIVATE_SETTINGS.add(POWERMENU_SETTINGS);
+            PRIVATE_SETTINGS.add(POWERMENU_LOCKDOWN);
+            PRIVATE_SETTINGS.add(POWERMENU_AIRPLANE);
+            PRIVATE_SETTINGS.add(POWERMENU_RESTART);
+            PRIVATE_SETTINGS.add(POWERMENU_RESTART_RECOVERY);
+            PRIVATE_SETTINGS.add(POWERMENU_POWER);
+            PRIVATE_SETTINGS.add(POWERMENU_USERS);
+            PRIVATE_SETTINGS.add(POWERMENU_LOGOUT);
+            PRIVATE_SETTINGS.add(POWERMENU_LOCKSCREEN);
         }
 
         /**
@@ -5009,6 +5140,17 @@ public final class Settings {
             VALIDATORS.put(BATTERY_LIGHT_FULL_COLOR, BATTERY_LIGHT_FULL_COLOR_VALIDATOR);
             VALIDATORS.put(BATTERY_LIGHT_REALLYFULL_COLOR, BATTERY_LIGHT_REALLYFULL_COLOR_VALIDATOR);
             VALIDATORS.put(FINGERPRINT_SUCCESS_VIB, FINGERPRINT_SUCCESS_VIB_VALIDATOR);
+            VALIDATORS.put(POWERMENU_SOUNDPANEL, POWERMENU_SOUNDPANEL_VALIDATOR);
+            VALIDATORS.put(POWERMENU_SCREENSHOT, POWERMENU_SCREENSHOT_VALIDATOR);
+            VALIDATORS.put(POWERMENU_SETTINGS, POWERMENU_SETTINGS_VALIDATOR);
+            VALIDATORS.put(POWERMENU_LOCKDOWN, POWERMENU_LOCKDOWN_VALIDATOR);
+            VALIDATORS.put(POWERMENU_AIRPLANE, POWERMENU_AIRPLANE_VALIDATOR);
+            VALIDATORS.put(POWERMENU_RESTART, POWERMENU_RESTART_VALIDATOR);
+            VALIDATORS.put(POWERMENU_RESTART_RECOVERY, POWERMENU_RESTART_RECOVERY_VALIDATOR);
+            VALIDATORS.put(POWERMENU_POWER, POWERMENU_POWER_VALIDATOR);
+            VALIDATORS.put(POWERMENU_USERS, POWERMENU_USERS_VALIDATOR);
+            VALIDATORS.put(POWERMENU_LOGOUT, POWERMENU_LOGOUT_VALIDATOR);
+            VALIDATORS.put(POWERMENU_LOCKSCREEN, POWERMENU_LOCKSCREEN_VALIDATOR);
         }
 
         /**
