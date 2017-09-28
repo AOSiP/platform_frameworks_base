795ae94a25e042c6c6c24229db59517e06b64496
Reifu Sato <reifu.x.sato@sony.com>
2017-09-28 15:10:09 +0900

    Collapse Notification panel after tapping CLEAR ALL button
    Usually, notification panel is automatically collapsed
    after tapping "CLEAR ALL" button but is not collapsed
    if all dismissable notifications are scrolled out.
    
    In this case, dismissed notification is still displayed
    until notification panel is once collapsed.
    And that "dismissed" notification causes several issues.
    
    To avoid this, collapse notification panel automatically
    after tapping "CLEAR ALL" button even when all dismissable
    notifications are scrolled out.
    
    Fixes: 67246876
    Test: manual - post ongoing and dismissable notifications,
                   and tap CLEAR ALL button.
    
    Change-Id: I048255cde6fdb4cedd2c3472e345ff63c744af68
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1540,6 +1540,11 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         };
 
+        if (hideAnimatedList.isEmpty()) {
+            animationFinishAction.run();
+            return;
+        }
+
         // let's disable our normal animations
         mStackScroller.setDismissAllInProgress(true);
 
