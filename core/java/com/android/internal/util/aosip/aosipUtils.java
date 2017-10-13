a98349a0b864b2c74bdca174272db383cd2c11e0
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not avail
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8

--- core/java/com/android/internal/util/aosip/aosipUtils.java
+++ core/java/com/android/internal/util/aosip/aosipUtils.java
@@ -16,8 +16,10 @@
 
 package com.android.internal.util.aosip;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -35,6 +37,8 @@ import android.view.KeyEvent;
 import android.view.InputDevice;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
+
+import java.util.List;
 
 public class aosipUtils {
 
@@ -121,5 +125,21 @@ public class aosipUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
