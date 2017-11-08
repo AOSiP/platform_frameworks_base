9caf0f47f18c8a25b17bf88e873b97b510e5bed7
Decad3nce <decad3nce@gmail.com>
2017-11-08 17:41:34 -0500

    Add heads up blacklist options
    Change-Id: I8182e94904b30ef82c0ae0bbd8e873328115c619

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4154,6 +4154,13 @@ public final class Settings {
        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
