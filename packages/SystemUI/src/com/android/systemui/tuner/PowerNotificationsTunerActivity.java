079725c0ce18783f5fb1de7068efc39bcf851dcb
ezio84 <brabus84@gmail.com>
2017-11-13 10:16:31 +0100

    SysUI tuner activities: fix SysUI fc on rotation
    e.g. NavBarTunerActivity.onCreate was calling
    super.onCreate(savedInstanceState), that is just NavBarTuner.onCreate,
    and that was calling:
    addTunable -> Dependency.get(TunerService.class).addTunable(tunable, keys)
    but Dependency.get is: return sDependency.getDependency(cls);
    so it needs sDependency to be created first with:
    initDependencies
    
    Change-Id: Iad34a87e5b7bca3b3e228c1c29d66490046bb577

--- packages/SystemUI/src/com/android/systemui/tuner/PowerNotificationsTunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/PowerNotificationsTunerActivity.java
@@ -27,8 +27,8 @@ public class PowerNotificationsTunerActivity extends TunerActivity {
     private static final String TAG_TUNER = "tuner";
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         Dependency.initDependencies(this);
+        super.onCreate(savedInstanceState);
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
             final String action = getIntent().getAction();
