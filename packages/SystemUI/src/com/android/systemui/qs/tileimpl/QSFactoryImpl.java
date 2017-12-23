2c2d9d30ea19ebf3907732e692f9e9533124cf5a
xyyx <xyyx@mail.ru>
2017-12-23 23:50:11 +0800

    SystemUI: Add tile for enable/disable HW keys
    Change-Id: I1408c2034fc82cde6d286b57d550cb54c4e39f9d
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -37,6 +37,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -122,6 +123,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new RebootTile(mHost);
             case "screenshot":
                 return new ScreenshotTile(mHost);
+            case "hwkeys":
+                return new HWKeysTile(mHost);
             case "heads_up":
                 return new HeadsUpTile(mHost);
             case "usb_tether":
