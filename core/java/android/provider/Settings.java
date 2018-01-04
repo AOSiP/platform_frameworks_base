8016b635df0aaa001e23d2500a977845c6e2c898
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    AOSiP: Reverse logic so user is enabling the actual
    notifications sound/vibration suppression
    
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5373,6 +5373,14 @@ public final class Settings {
         public static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
