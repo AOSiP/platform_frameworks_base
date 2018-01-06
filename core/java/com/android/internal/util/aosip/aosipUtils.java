49c0074b64811e58086a2af7c6f228abca90b365
maxwen <max.weninger@gmail.com>
2018-01-06 21:23:26 +0100

    AOSiPUtils: Add isAppInstalled
    Change-Id: I15189741359537e0790a97e3ae832f503388ef7f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/com/android/internal/util/aosip/aosipUtils.java
+++ core/java/com/android/internal/util/aosip/aosipUtils.java
@@ -35,6 +35,16 @@ import android.view.KeyEvent;
 
 public class aosipUtils {
 
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
