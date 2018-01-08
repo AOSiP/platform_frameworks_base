37b897723863854987dd786fe3b4ab9210dad461
maxwen <max.weninger@gmail.com>
2018-01-08 02:52:58 +0100

    Screenshot tile: persist screenshot type after reboot
    save state set in qs tile into settings and use that
    after boot
    
    Change-Id: Ieca6148862386cef01767d43938bb803aab282cb
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4103,6 +4103,13 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
