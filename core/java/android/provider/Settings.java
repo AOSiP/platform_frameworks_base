74bbf9cd3454fa556874315e32f066ded72a22da
mydongistiny <jaysonedson@gmail.com>
2018-01-23 23:59:30 +0000

    Allow USB mode dialog to be enabled/disabled [2/2]
    Change-Id: I7d1052057e4b3b4d78fdbf8179cddb655a43cdeb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4533,6 +4533,11 @@ public final class Settings {
          */
         public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
                 "notification_guts_kill_app_button";
+ 
+        /**
+         * @hide
+         */
+        public static final String SHOW_USB_MODE_DIALOG = "show_usb_mode_dialog";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
