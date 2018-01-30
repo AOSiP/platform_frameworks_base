1b01a21e4fbd07c7aef26440679cd6230dfe2206
xyyx <xyyx@mail.ru>
2018-01-30 14:35:41 +0800

    Add toggle for face auto unlock (1/2)
    Change-Id: I2974a2b44ee977ccc973c083bef9777f94eed1af

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7819,6 +7819,13 @@ public final class Settings {
          */
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
+	/**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
         /**
          * This are the settings to be backed up.
          *
