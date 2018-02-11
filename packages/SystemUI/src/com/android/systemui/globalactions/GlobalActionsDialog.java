9b52e9dd2fd1aed0e1152ab9f4e82502fa70a684
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-11 00:27:19 -0500

    Disallow extra power menu items when the device is in lock task mode
    "Lock task mode" is the mode that the device enters when you pin a app from
    recents.
    
    In this mode the user is restricted from using Fling, Pulse and even Smartbar
    as well as QS panel. The navbar reverts back to stock and uses the stock
    configuration. This is by design. We don't want the user to use any custom
    features because this mode is intended to lock a user in an app. They can
    obviously exit out of it but that's on them.
    
    With all this said, we want to restrict the power menu items to just the stock
    configuration as well. Screenshot, Airplane and Advanced reboot will be hidden
    while in this mode.
    
    - We're adapting the original commit to our needs
    
    Original commit -
    https://github.com/PixelExperience/frameworks_base/commit/6a4a9e04668b5e62d626341e72a0dc8dcea85a7b
    
    Change-Id: I024a6d6972b9e943d6f3f5fae0b582945dc6519a
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -462,7 +462,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                 mItems.add(new PowerAction());
             } else if (GLOBAL_ACTION_KEY_AIRPLANE.equals(actionKey)) {
                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_AIRPLANE, 0, UserHandle.USER_CURRENT) != 0) {
+                        Settings.System.POWERMENU_AIRPLANE, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(mAirplaneModeOn);
                 }
             } else if (GLOBAL_ACTION_KEY_BUGREPORT.equals(actionKey)) {
@@ -494,12 +494,12 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             } else if (GLOBAL_ACTION_KEY_ADVANCED.equals(actionKey)) {
                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.POWERMENU_REBOOT, 1, UserHandle.USER_CURRENT) == 1 && Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_ADVANCED_REBOOT, 0, UserHandle.USER_CURRENT) != 0) {
+                        Settings.System.POWERMENU_ADVANCED_REBOOT, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(mShowAdvancedToggles);
                 }
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_SCREENSHOT, 0, UserHandle.USER_CURRENT) != 0) {
+                        Settings.System.POWERMENU_SCREENSHOT, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(new ScreenshotAction());
                 }
             } else {
@@ -1708,6 +1708,14 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             }
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform hot reboot", e);
+        }
+    }
+
+    private boolean isInLockTaskMode() {
+        try {
+            return ActivityManagerNative.getDefault().isInLockTaskMode();
+        } catch (RemoteException e) {
+            return false;
         }
     }
 }
