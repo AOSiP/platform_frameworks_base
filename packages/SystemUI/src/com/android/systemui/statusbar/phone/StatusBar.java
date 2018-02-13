ccc995de98e950f9a9e3ac36f0cb7dca0ce62920
ezio84 <brabus84@gmail.com>
2018-02-13 04:54:00 -0600

    BatteryMeterView: add large icon option [1/2]
    Change-Id: I3ee52eb69d4588e3b01ca8e1cefc42f34a35c6c8
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5921,7 +5921,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE),
                     false, this, UserHandle.USER_ALL);
-	   update();
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BIG_BATTERY_ICON),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -5938,7 +5940,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.SHOW_BATTERY_PERCENT))
                     || uri.equals(Settings.Secure.getUriFor(
-                    Settings.Secure.STATUS_BAR_BATTERY_STYLE))) {
+                    Settings.Secure.STATUS_BAR_BATTERY_STYLE))
+                    || uri.equals(Settings.Secure.getUriFor(
+                    Settings.Secure.STATUS_BAR_BIG_BATTERY_ICON))) {
                 setStatusBarOptions();
             }
         }
