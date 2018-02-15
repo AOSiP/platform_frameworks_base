62a1781fe1db2af8e86bf3393e28480ac0f5cd0a
ezio84 <brabus84@gmail.com>
2018-02-15 13:28:03 +0100

    BatteryMeterView: add padding depending on the clock position
    Change-Id: Iaf507a676ea19d99918460bc70ba0ff354317314
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -38,6 +38,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
+import com.android.systemui.BatteryMeterView;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.SignalClusterView;
 import com.android.systemui.statusbar.phone.StatusBarIconController.DarkIconManager;
@@ -70,6 +71,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private View mClock;
     private View mLeftClock;
     private final Handler mHandler = new Handler();
+    private BatteryMeterView mBattery;
     private ContentResolver mContentResolver;
 
     private class AOSiPSettingsObserver extends ContentObserver {
@@ -116,6 +118,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         public void updateSettings(boolean animate) {
             ((Clock)mClock).updateSettings();
             ((Clock)mLeftClock).updateSettings();
+            mBattery.updateSettings(true);
      }
 
     private SignalCallback mSignalCallback = new SignalCallback() {
@@ -154,6 +157,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mSignalClusterView = mStatusBar.findViewById(R.id.signal_cluster);
         mClock = mStatusBar.findViewById(R.id.clock);
         mLeftClock = mStatusBar.findViewById(R.id.left_clock);
+        mBattery = mStatusBar.findViewById(R.id.battery);
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mSignalClusterView);
         // Default to showing until we know otherwise.
         showSystemIconArea(false);
