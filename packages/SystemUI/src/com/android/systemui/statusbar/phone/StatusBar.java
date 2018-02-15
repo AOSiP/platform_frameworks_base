10f5b65aff3db3ccb0f0ea7434b8d76c4db733ff
ezio84 <brabus84@gmail.com>
2018-02-15 13:28:03 +0100

    Circle battery: add a bit of padding when Right clock is enabled
    or quick statusbar header is showing
    
    Change-Id: If382bcf1fbf4699e60a26b89e4ccdb15a9be6cda
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -6348,7 +6348,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
-    private void updateBatterySettings() {
+    public void updateBatterySettings() {
         if (mStatusBarView != null) {
             mStatusBarView.updateBatterySettings();
         }
