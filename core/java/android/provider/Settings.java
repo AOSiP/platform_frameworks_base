7ee2b8e6ef6ebb282ae94845a2a28418c48dd46b
SpiritCroc <spiritcroc@gmail.com>
2018-02-23 15:51:30 +0100

    Return of the ticker scroll [1/2]
    Change-Id: Ibedf1541e16e3304d68c2c10eb43a07f3c309c78
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4535,6 +4535,14 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Ticker animation
+         * 0: Fade animation
+         * 1: Scrolling ticker
+         */
+        public static final String STATUS_BAR_TICKER_ANIMATION_MODE =
+                "status_bar_ticker_animation_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
