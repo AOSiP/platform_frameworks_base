f7d7879a5ce09008f4e200f84c3241af8aa3e553
ezio84 <brabus84@gmail.com>
2018-03-08 14:04:27 +0100

    Statusbar text battery: use  ⚡  instead of ~ when charging
    Change-Id: I8e18a2c0de632104f4078069532cc1ae136d8896
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -78,6 +78,8 @@ public class BatteryMeterView extends LinearLayout implements
     private float mDarkIntensity;
     private int mUser;
 
+    private boolean mCharging;
+
     private final Context mContext;
     private final int mFrameColor;
 
@@ -213,7 +215,12 @@ public class BatteryMeterView extends LinearLayout implements
 
     private void updatePercentText() {
         if (mBatteryPercentView != null) {
-            mBatteryPercentView.setText(
+            // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+            // to load its emoji colored variant with the uFE0E flag
+            String bolt = "\u26A1\uFE0E";
+            CharSequence mChargeIndicator =
+                    mCharging && mStyle == BatteryMeterDrawableBase.BATTERY_STYLE_TEXT ? (bolt + " ") : "";
+            mBatteryPercentView.setText(mChargeIndicator +
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
         }
     }
