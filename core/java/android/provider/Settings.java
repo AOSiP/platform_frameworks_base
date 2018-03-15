10731e5513df574e6f9039d9d08c963290d95169
ezio84 <brabus84@gmail.com>
2018-03-16 00:01:53 +0100

    Choose a custom editor app for screenshot notification edit button [1/2]
    Change-Id: Ie8e6acde0fd8ad198bb79e7cfec22490d0522890

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4584,6 +4584,12 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Stores the app to be launched if the user taps on the screenshot notification edit button
+         * @hide
+         */
+        public static final String SCREENSHOT_EDIT_USER_APP = "screenshot_edit_user_app";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
