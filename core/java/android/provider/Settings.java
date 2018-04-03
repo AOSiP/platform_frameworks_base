4d413cc0e0568d3f4db9456b36c0276fe1866884
Josh <joshfox87@gmail.com>
2018-04-03 10:09:25 -0400

    Themes: Add our BlackAF variant to styles
    Change-Id: Ie8588cbfc1e1363019007a0116198d9a058bf8d6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4406,6 +4406,7 @@ public final class Settings {
          * 0 - wallpaper based
          * 1 - force light
          * 2 - force dark
+         * 3 - force blackaf
          * @hide
          */
         public static final String SYSTEM_UI_THEME = "system_ui_theme";
