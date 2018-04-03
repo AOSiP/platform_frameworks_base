2f8c2c2be1f7c42a581b5c991611db229cf8962f
Preeti Nagar <pnagar@codeaurora.org>
2018-04-03 16:34:25 +0530

    SEEMP: framework instrumentation and AppProtect features
    - Changes for Android-P porting
    - Instrument framework files to log events of interest for QSSP.
    - Fix the String out of boundary.
    - Add optional package verifier to PackageManagerService
      - In additional to the required and sufficient package verifiers,
        add support for an optional verifier in PackageManager. The
        optional verifier will act as a required verifier if a required
        verifier is not found in the system. It will act as a secondary
        verifier if a required verifier is present.
    - Add auth framework for outgoing SMS messages.
      - Add the APIs required to register an authorization agent in order
        to allow/reject outgoing SMS messages and the core service that
        implements the sms security model.
    - Do not connect to seempdw socket in zygote
      - Do not connect to seempdw socket in zygote. The socket
        initialization is skipped if the parent process id is 1, which is
        always the case for zygote.
    - Moved instrumentation point to PackageInstaller and some
      minor fixes.
    - Add action for optional package verifier along other legacy
      actions to handle tightened StrictMode policy in Android P.
    
    Conflicts:
    	Android.mk
    	core/java/android/accounts/AccountManager.java
    	core/java/android/app/Instrumentation.java
    	core/java/android/bluetooth/BluetoothAdapter.java
    	core/jni/Android.mk
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    	services/core/java/com/android/server/pm/PackageManagerService.java
    	telephony/java/android/telephony/Telephony.java
    	tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/view/WindowManagerImpl.java
    	core/java/android/app/Instrumentation.java
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    
    Change-Id: I38a8ace9943fae0ad2ac7bc2932f3deb790359a6
    CRs-Fixed: 2219016

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -870,6 +870,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setPreviewCallback(PreviewCallback cb) {
+        android.util.SeempLog.record(66);
         mPreviewCallback = cb;
         mOneShot = false;
         mWithBuffer = false;
@@ -898,6 +899,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setOneShotPreviewCallback(PreviewCallback cb) {
+        android.util.SeempLog.record(68);
         mPreviewCallback = cb;
         mOneShot = true;
         mWithBuffer = false;
@@ -938,6 +940,7 @@ public class Camera {
      * @see android.media.MediaActionSound
      */
     public final void setPreviewCallbackWithBuffer(PreviewCallback cb) {
+        android.util.SeempLog.record(67);
         mPreviewCallback = cb;
         mOneShot = false;
         mWithBuffer = true;
@@ -1503,6 +1506,7 @@ public class Camera {
      */
     public final void takePicture(ShutterCallback shutter, PictureCallback raw,
             PictureCallback jpeg) {
+        android.util.SeempLog.record(65);
         takePicture(shutter, raw, null, jpeg);
     }
     private native final void native_takePicture(int msgType);
@@ -1541,6 +1545,7 @@ public class Camera {
      */
     public final void takePicture(ShutterCallback shutter, PictureCallback raw,
             PictureCallback postview, PictureCallback jpeg) {
+        android.util.SeempLog.record(65);
         mShutterCallback = shutter;
         mRawImageCallback = raw;
         mPostviewCallback = postview;
