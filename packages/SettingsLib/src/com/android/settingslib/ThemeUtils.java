f5657864f3859f50f087c8a3fe679d595f3279e8
Akhil Narang <akhilnarang.1999@gmail.com>
2018-04-04 20:40:39 +0530

    Fruity Pebbles: cleanup the code a bit
    Create a ThemeUtils class in SettingsLib to reduce duplicated code
    General cleanup of some code for the accents
    
    Change-Id: I43844a505ff3a644d1ad7de37c7c8072458d0cf3
    Co-authored-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ packages/SettingsLib/src/com/android/settingslib/ThemeUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 AOSiP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.settingslib;
+
+import android.content.Context;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+
+public class ThemeUtils {
+    private static final String TAG = "ThemeUtils";
+
+    private static IOverlayManager mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+    private static int mCurrentUserId = UserHandle.USER_CURRENT;
+
+     // Check if black accent should be used
+    public static boolean canUseBlackAccent() {
+        return !isUsingDarkTheme() && !isUsingBlackAFTheme();
+    }
+
+    public static boolean isUsingDarkTheme() {
+        return isOverlayEnabled("com.android.system.theme.dark");
+    }
+
+    public static boolean isUsingBlackAFTheme() {
+        return isOverlayEnabled("com.android.system.theme.blackaf");
+    }
+
+    // Checks if the overlay is enabled
+    public static boolean isOverlayEnabled(String packageName) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = mOverlayManager.getOverlayInfo(packageName,
+                    mCurrentUserId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
+}
