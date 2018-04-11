a67012755b86d44fc4069e86b1cc405e7dd012ef
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    neobuddy89: Fixes and simplify.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I5ef3f98e46a3ec29760daebf130166e38be3deae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5174,6 +5174,12 @@ public final class Settings {
                 "status_bar_file_header_image";
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
