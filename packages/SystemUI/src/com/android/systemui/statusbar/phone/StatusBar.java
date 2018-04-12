8faeaa18dec4966f9852db3183af46824a501c8e
ezio84 <brabus84@gmail.com>
2018-04-12 13:10:38 +0200

    Screen off skip track: add Spotify/kdeconn./others remote session compatibility
    it's nice to be able to skip tracks with volume rocker even when using the app
    as a remote control (e.g. Spotify playing on pc, chromecast or ps4 and using
    app running on the phone as remote controller).
    
    Change-Id: I0c26cace79e30166c9d6e9995a6c994579958971
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3242,6 +3242,36 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void handleSystemKey(int key) {
         if (SPEW) Log.d(TAG, "handleNavigationKey: " + key);
+
+        if (KeyEvent.KEYCODE_MEDIA_PREVIOUS == key || KeyEvent.KEYCODE_MEDIA_NEXT == key) {
+            if (mMediaSessionManager != null) {
+                final List<MediaController> sessions
+                        = mMediaSessionManager.getActiveSessionsForUser(
+                                null,
+                                UserHandle.USER_ALL);
+                for (MediaController aController : sessions) {
+                    if (PlaybackState.STATE_PLAYING ==
+                            getMediaControllerPlaybackState(aController)) {
+                        long when = SystemClock.uptimeMillis();
+                        final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, key, 0);
+                        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                aController.dispatchMediaButtonEvent(evDown);
+                            }
+                        });
+                        mHandler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                aController.dispatchMediaButtonEvent(evUp);
+                            }
+                        }, 20);
+                    }
+                }
+            }
+            return;
+        }
         if (!panelsEnabled() || !mKeyguardMonitor.isDeviceInteractive()
                 || mKeyguardMonitor.isShowing() && !mKeyguardMonitor.isOccluded()) {
             return;
@@ -3269,7 +3299,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 clearAllNotifications(KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT == key ? true : false);
             }
         }
-
     }
 
     boolean panelsEnabled() {
