14c710e9a2db5bf251f61432bdd9e33d450cc647
ezio84 <brabus84@gmail.com>
2018-06-12 10:30:46 +0200

    Cleanup Ambient music ticker [1/2]
    remove useless normal forced mode
    skip useless setDozing calls
    enable by default
    
    Change-Id: Ia0733ea33480ed4ae1a48fb0db92d670daded884
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -743,7 +743,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     public void triggerAmbientForMedia() {
-        if (mAmbientMediaPlaying == 2 || mAmbientMediaPlaying == 3) {
+        if (mAmbientMediaPlaying == 2) {
             mDozeServiceHost.fireNotificationMedia();
         }
     }
@@ -6220,7 +6220,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                         // Otherwise just show the always-on screen.
                         setPulsing(pulsingEntries);
                     }
-                    setOnPulseEvent((mAmbientMediaPlaying == 3 ? reason : -1), true);
+                    setOnPulseEvent((mAmbientMediaPlaying == 2 ? reason : -1), true);
                 }
 
                 @Override
@@ -6730,7 +6730,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private void setForceAmbient() {
         mAmbientMediaPlaying = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0,
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 2,
                 UserHandle.USER_CURRENT);
         if (isAmbientContainerAvailable()) {
             ((AmbientIndicationContainer)mAmbientIndicationContainer).setIndication(mMediaMetadata, null);
