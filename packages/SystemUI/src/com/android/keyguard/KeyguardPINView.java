9ad1212ba22a392e7aa59f88924bf09c143b84a4
Alex <alex@dirtyunicorns.com>
2018-06-21 00:14:04 -0500

    Make scramble PIN layout Multi-User friendly
    Change-Id: I564eb6c61b5c8360f6651d05e124cd7c7a6a6676
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -18,7 +18,6 @@ package com.android.keyguard;
 
 import android.content.Context;
 import android.os.UserHandle;
-
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -139,8 +138,8 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         setButtonVisibility(getOkButton(), !quickUnlock);
 
         //Scramble Pin Layout
-	boolean scramblePin = (Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0) == 1);
+        boolean scramblePin = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0, UserHandle.USER_CURRENT) == 1;
 
         if (scramblePin) {
             Collections.shuffle(sNumbers);
