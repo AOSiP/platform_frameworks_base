4b9616bfd3426399499ac9e3228f5662b97c23b4
ezio84 <brabus84@gmail.com>
2018-06-22 17:28:16 +0200

    Add dynamic Immersive mode tile
    * On top of existing AOSP policy control code
    
    - Immersive statusbar
    - Immersive navbar
    - Full immersive
    - Tap to cycle modes
    - Long press to enable the selected immersive mode
    - If the current immersive mode is enabled, long pressing again the tile
      will disable immersive so no need to cycle the tile to go to the "off"
      position
    - Tile icon will be set to active or unactive state according to the
    current mode
    
    *DU Edits*
    - Switched to using switch case vs if/else statements (easier this way)
    - Changed QS tiles to match expanded desktop ones (credit mik9)
    https://github.com/DirtyUnicorns/android_packages_apps_DU-Tweaks/commit/60ffd67cf1f263fff5bcb1f572c8569b8bfe6486
    
    Change-Id: I1747b7f4077247014ca5e2a160712354df6e023e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,6 +35,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
@@ -122,6 +123,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new UsbTetherTile(mHost);
             case "screenrecord":
                 return new ScreenrecordTile(mHost);
+            case "immersive":
+                return new ImmersiveTile(mHost);
         }
 
         // Intent tiles.
