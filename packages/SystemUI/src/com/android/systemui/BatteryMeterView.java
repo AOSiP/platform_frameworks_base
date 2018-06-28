fcb9ead84d866b36d2ec3e8d8d4c19e1d3f79212
tsubus <tsubus@ilwt.org>
2018-06-28 11:28:08 +0200

    Option to show battery percentage when charging [2/2]
    - optional when using battery percentage inside
    
    Change-Id: I1d789ef21a2433d28fd3df204757f585fda42bfd
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -179,7 +179,11 @@ public class BatteryMeterView extends LinearLayout implements
 
     @Override
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+        boolean wasCharging = mCharging;
         mCharging = pluggedIn;
+        if (mPercentStyle == 3 && wasCharging != pluggedIn) {
+            updateShowPercent();
+        }
         mDrawable.setBatteryLevel(level);
         mDrawable.setCharging(pluggedIn);
         mLevel = level;
@@ -216,6 +220,13 @@ public class BatteryMeterView extends LinearLayout implements
         int style = mPercentStyle;
         if (mForceShowPercent) {
             style = 1; // Default view
+        }
+        if (style == 3) {
+            if (mCharging) {
+                style = 1;
+            } else {
+                style = 2;
+            }
         }
         switch (style) {
             case 1:
