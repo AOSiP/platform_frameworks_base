fa9f958b70cc4baed91ccf3d541e932c403c6bdc
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 17:09:54 +0200

    SystemUI: Add tunables for clock position
    AOSiP: Use Settings.Secure
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Aug 1 18:10:39 2018 +0200
    
        SystemUI: Initialize mClockVisibleByUser to android:visibility
    
        * Fixes ~ triple clock ~ after leaving from Google SUW.
    
        Change-Id: I3ceea4400b3057e2b6b1f221f9074b1d451133ea
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sun Jul 29 14:19:08 2018 +0200
    
        SystemUI: Fix default clock blacklist value
    
        Change-Id: Ibdc4f6a707569ddb417854760cf579daca989ebb
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Tue Apr 2 20:56:25 2019 +0200
    
        SystemUI: Don't hide right clock when heads up is visible
    
        * https://gitlab.com/LineageOS/issues/android/issues/436
    
        Change-Id: I85a6cd2dff79e6b97d8a7db552c22d611bf25dbd
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu Sep 27 12:24:19 2018 +0200
    
        SystemUI: Fix clock icon blacklist handling
    
        * This makes sure clock isn't on blacklist before
          calling showClock(animate);
    
        Change-Id: I7f77a83aa6c109e1682d764fb22701375ae5ba1e
    
    Author: Hendrik Hagendorn <git@finnq.de>
    Date:   Sun Jan 15 10:00:12 2017 +0100
    
        Tuner: Move clock seconds option to status bar settings
    
        Change-Id: I532c179f06b934009d48433e50b69660247ee295
    
    Change-Id: Icdc039d13fd2676c1eb979a81fb77f9a6ec59b4a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8932,6 +8932,17 @@ public final class Settings {
          */
         public static final String PEOPLE_STRIP = "people_strip";
 
+       /**
+         * Whether to show the clock in the right or left position or show it in the center
+         * 0: show the clock in the right position (LTR)
+         * 1: show the clock in the center
+         * 2: show the clock in the left position (LTR)
+         * default: 0
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK = "status_bar_clock";
+
         /**
          * Whether or not to enable media resumption
          * When enabled, media controls in quick settings will populate on boot and persist if
