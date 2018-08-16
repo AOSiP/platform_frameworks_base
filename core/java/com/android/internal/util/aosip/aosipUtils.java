4bfcd401c2229681792014c2db4f1df392a3274e
maxwen <max.weninger@gmail.com>
2018-08-16 20:53:46 +0200

    Navbar: Add navigation bar visibility control
    Change-Id: Ifa324726d857c5a9351f978ef03480900e468be0

--- core/java/com/android/internal/util/aosip/aosipUtils.java
+++ core/java/com/android/internal/util/aosip/aosipUtils.java
@@ -427,4 +427,29 @@ public class aosipUtils {
             }
         }
     }
+
+    public static boolean deviceSupportNavigationBar(Context context) {
+        return deviceSupportNavigationBarForUser(context, UserHandle.USER_CURRENT);
+    }
+
+    public static boolean deviceSupportNavigationBarForUser(Context context, int userId) {
+        final boolean showByDefault = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        final int hasNavigationBar = Settings.System.getIntForUser(
+                context.getContentResolver(),
+                Settings.System.FORCE_SHOW_NAVBAR, -1, userId);
+
+        if (hasNavigationBar == -1) {
+            String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                return false;
+            } else if ("0".equals(navBarOverride)) {
+                return true;
+            } else {
+                return showByDefault;
+            }
+        } else {
+            return hasNavigationBar == 1;
+        }
+    }
 }
