ec8e303d1647f2e896d2787d732eb0a785cc7e22
Alex Cruz <alex@dirtyunicorns.com>
2018-09-20 09:23:07 -0500

    Add moar overlay [1/2]
    - Settings
    - SettingsIntelligence
    
    Change-Id: I77beab7e08593cdf8c872caa04d772f705bb37f6
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3942,6 +3942,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             try {
                 mOverlayManager.setEnabled("com.android.system.theme.dark",
                         useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                mOverlayManager.setEnabled("com.android.settings.theme.dark",
+                        useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                mOverlayManager.setEnabled("com.android.settings.intelligence.theme.dark",
+                        useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                 if (useDarkTheme) {
                     unloadStockDarkTheme();
                 }
