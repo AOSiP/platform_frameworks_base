17832e83bbd6531342b7626e8ad22b2d7cf3ab52
Alex Cruz <alex@dirtyunicorns.com>
2018-09-24 19:21:59 -0500

    Fruity Pebbles
    In the commit below we added the ability to change between Dark/Light and wallpaper
    based themes.
    
    d5de41e
    
    Since then we've extended this to Settings, DU-Tweaks, Changelog and a few other
    apps in system. We've added more styles and have gone all in on this theme thing.
    
    One thing that was missing was the ability to change the system accent. While the
    pixel accent was good enough, many users had asked us to add in the ability to
    change the accents because they wanted something different. Several ROMs have gone
    about this same thing in their own way but we didn't. We wanted to use what we already
    had and just add on to it.
    
    This commit mirrors what maxwen did but with the accent, so thanks to him for that!
    
    Change-Id: I5bc383d749d34ba10c0cd96153d17e706b9dac3c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4417,6 +4417,13 @@ public final class Settings {
         public static final String SYSTEM_THEME = "system_theme";
 
         /**
+         * Select various accents to go along with system themes
+         *
+         * @hide
+         */
+        public static final String ACCENT_PICKER = "accent_picker";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
