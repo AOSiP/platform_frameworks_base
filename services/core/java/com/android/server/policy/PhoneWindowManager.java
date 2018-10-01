f72c2584c57ad0142b885940b299df005c66b765
xyyx <xyyx@mail.ru>
2018-10-01 21:31:43 +0800

    HwKeys: Disable haptic in Ambient or Always on
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3921,7 +3921,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && event.getRepeatCount() == 0;
 
         if (!virtualKey) {
-            if (isHwKeysDisabled() || keyguardOn()) {
+            if (isHwKeysDisabled() || keyguardOn() || isDozeMode()) {
                 useHapticFeedback = false;
             }
         }
