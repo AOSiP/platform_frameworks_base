da229cb849b79c09326a0b526f4101e2bf5d7635
Shubham Singh <coolsks94@gmail.com>
2018-10-08 17:09:12 +0530

    fwb: Allow users to switch between Pie/Oreo recents [1/3]
    I personally don't like Pie's recent style built in launcher3,
    Hence allow to switch between recents components(Quickstep/SystemUI) without much hassle.
    Other two parts of commit in Extensions and Settings will also take care of swipe up gesture.
    If it is enabled it will be disabled and removed from Settings>System>Gestures, if in case Oreo style recents is selected by user.
    
    Commit's logic is based on https://github.com/PotatoProject/frameworks_base/commit/74f8b464a944cff9f382148521aab52d0c054136 , so huge thanks to @deletescape
    
    Change-Id: I2c0657bb798c515bd946bf1885d355520a8b98c0
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4846,6 +4846,18 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Which component to use for Recents UI
+         * 0 - Pie Recents (Quickstep)
+         * 1 - Oreo Recents (SystemUI)
+         * @hide
+         */
+        public static final String RECENTS_COMPONENT = "recents_component";
+
+        /** @hide */
+        public static final Validator RECENTS_COMPONENT_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4933,6 +4945,7 @@ public final class Settings {
             BATTERY_LIGHT_REALLYFULL_COLOR,
             FINGERPRINT_SUCCESS_VIB,
             SHOW_FOURG,
+            RECENTS_COMPONENT
         };
 
         /**
@@ -5085,6 +5098,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_REALLYFULL_COLOR);
             PRIVATE_SETTINGS.add(FINGERPRINT_SUCCESS_VIB);
             PRIVATE_SETTINGS.add(WEATHER_LOCKSCREEN_UNIT);
+            PRIVATE_SETTINGS.add(RECENTS_COMPONENT);
         }
 
         /**
@@ -5205,6 +5219,7 @@ public final class Settings {
             VALIDATORS.put(BATTERY_LIGHT_REALLYFULL_COLOR, BATTERY_LIGHT_REALLYFULL_COLOR_VALIDATOR);
             VALIDATORS.put(FINGERPRINT_SUCCESS_VIB, FINGERPRINT_SUCCESS_VIB_VALIDATOR);
             VALIDATORS.put(WEATHER_LOCKSCREEN_UNIT, WEATHER_LOCKSCREEN_UNIT_VALIDATOR);
+            VALIDATORS.put(RECENTS_COMPONENT,RECENTS_COMPONENT_VALIDATOR);
         }
 
         /**
