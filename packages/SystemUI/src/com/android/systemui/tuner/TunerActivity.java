38eaf69808b8107e85bb39bb40c23cd0818193ff
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-10-17 12:24:22 +0000

    SystemUI: TunerActivity: Fix SystemUI force close on rotation
    Change-Id: Iae98956db8424b0b04776be3b213e031916017bd
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -36,8 +36,8 @@ public class TunerActivity extends SettingsDrawerActivity implements
     private static final String TAG_TUNER = "tuner";
 
     protected void onCreate(Bundle savedInstanceState) {
-        Dependency.initDependencies(this);
         super.onCreate(savedInstanceState);
+        Dependency.initDependencies(this);
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
             final String action = getIntent().getAction();
