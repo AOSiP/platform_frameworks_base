a115a8ce319089541b99cc8714b67441f9ba06e5
ezio84 <brabus84@gmail.com>
2018-10-28 16:32:28 +0100

    CustomDoze: fix custom gestures not working if default pulsing is disabled [1/2]
    we must be able to trigger ambient if pulse on notification
    is disabled but we still want to use tilt, handwave or pocket mode
    
    Change-Id: I05adb0f88b0aaca39d954dcec7c33c41c579b90b
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4715,6 +4715,24 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * Custom Ambient tilt gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_TILT_GESTURE = "custom_ambient_tilt_gesture";
+
+        /**
+         * Custom Ambient handwave gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_HANDWAVE_GESTURE = "custom_ambient_handwave_gesture";
+
+         /**
+         * Custom Ambient pocketmode gesture
+         * @hide
+         */
+        public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
