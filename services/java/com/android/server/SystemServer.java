a8ddb2f756298a5668c28cbc3a25982665e4135c
Camvar97 <cam.avarley@gmail.com>
2018-11-13 20:45:17 +0000

    Fonts: patched a few need files for boot
    Fontservice: move some lines around in hopes of a boot
    
    fix?
    
    Fontservice: Remove final variable and move load oarder for zygote
    
    Finally got it woking
    
    Change-Id: I4c0b65396371dc73ad9d05beacbdde72a7793b0c

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -703,12 +703,7 @@ public final class SystemServer {
             startSensorService();
             traceLog.traceEnd();
         }, START_SENSOR_SERVICE);
-	
-	// Manages fonts
-        traceBeginAndSlog("StartFontService");
-        mSystemServiceManager.startService(FONT_SERVICE_CLASS);
-        traceEnd();
-    }
+	}
 
     /**
      * Starts some essential services that are not tangled up in the bootstrap process.
@@ -843,6 +838,10 @@ public final class SystemServer {
             // Now that SettingsProvider is ready, reactivate SQLiteCompatibilityWalFlags
             SQLiteCompatibilityWalFlags.reset();
             traceEnd();
+
+			traceBeginAndSlog("StartFontService");
+        	mSystemServiceManager.startService(FONT_SERVICE_CLASS);
+        	traceEnd();
 
             // Records errors and logs, for example wtf()
             // Currently this service indirectly depends on SettingsProvider so do this after
