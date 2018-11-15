6e35ea698b7bba8edb130481d83a646eacbe6b9c
ezio84 <brabus84@gmail.com>
2018-11-15 01:12:39 +0100

    Add big circle battery[1/2]
    Change-Id: I330d9e6767f960b8081dcc870a61e21e6d30e10e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -360,8 +360,10 @@ public class BatteryMeterView extends LinearLayout implements
         res.getValue(R.dimen.status_bar_icon_scale_factor, typedValue, true);
         float iconScaleFactor = typedValue.getFloat();
 
-        int batteryHeight = res.getDimensionPixelSize(R.dimen.status_bar_battery_icon_height);
-        int batteryWidth = res.getDimensionPixelSize(R.dimen.status_bar_battery_icon_width);
+        int batteryHeight = res.getDimensionPixelSize(
+                isBigCircleBattery() ? R.dimen.status_bar_battery_bigcircle_icon_height : R.dimen.status_bar_battery_icon_height);
+        int batteryWidth = res.getDimensionPixelSize(
+                isBigCircleBattery() ? R.dimen.status_bar_battery_bigcircle_icon_width : R.dimen.status_bar_battery_icon_width);
         int marginBottom = res.getDimensionPixelSize(R.dimen.battery_margin_bottom);
 
         LinearLayout.LayoutParams scaledLayoutParams = new LinearLayout.LayoutParams(
@@ -457,9 +459,14 @@ public class BatteryMeterView extends LinearLayout implements
                     mBatteryIconView = new ImageView(mContext);
                     mBatteryIconView.setImageDrawable(mDrawable);
                     final MarginLayoutParams mlp = new MarginLayoutParams(
-                            getResources().getDimensionPixelSize(R.dimen.status_bar_battery_icon_width),
-                            getResources().getDimensionPixelSize(R.dimen.status_bar_battery_icon_height));
-                    mlp.setMargins(0, 0, 0, getResources().getDimensionPixelOffset(R.dimen.battery_margin_bottom));
+                            isBigCircleBattery()
+                            ? getResources().getDimensionPixelSize(R.dimen.status_bar_battery_bigcircle_icon_width)
+                            : getResources().getDimensionPixelSize(R.dimen.status_bar_battery_icon_width),
+                            isBigCircleBattery()
+                            ? getResources().getDimensionPixelSize(R.dimen.status_bar_battery_bigcircle_icon_height)
+                            : getResources().getDimensionPixelSize(R.dimen.status_bar_battery_icon_height));
+                    mlp.setMargins(0, 0, 0,
+                            getResources().getDimensionPixelOffset(R.dimen.battery_margin_bottom));
                     addView(mBatteryIconView, mlp);
                 }
                 break;
@@ -472,6 +479,11 @@ public class BatteryMeterView extends LinearLayout implements
     private boolean isCircleBattery() {
         return getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_CIRCLE
                 || getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_DOTTED_CIRCLE;
+    }
+
+    private boolean isBigCircleBattery() {
+        return mDrawable.getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_BIG_CIRCLE
+                || mDrawable.getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_BIG_DOTTED_CIRCLE;
     }
 
     private int getMeterStyle() {
