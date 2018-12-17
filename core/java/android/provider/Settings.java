0855d0b34f509692e6dd54ec3f26c246fc1d0f61
a.derendyaev <a.derendyaev@magdv.com>
2018-12-17 16:28:44 +0700

    extended audio panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4263,6 +4263,24 @@ public final class Settings {
          */
         public static final String QS_TILE_HIDE_TITLE = "qs_tile_hide_title";
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * Whether to display the reboot option in the power menu
          *
