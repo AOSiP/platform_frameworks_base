DirtyUnicorns/android_frameworks_base

Move power menu methods to our Utils class
Change-Id: I5fcb1f2a5f7e6e379d906b3151ad9bbee1a820b8

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -22,9 +22,7 @@ import static com.android.internal.widget.LockPatternUtils.StrongAuthTracker.STR
 import static com.android.internal.widget.LockPatternUtils.StrongAuthTracker.STRONG_AUTH_REQUIRED_AFTER_USER_LOCKDOWN;
 
 import android.app.ActivityManager;
-import android.app.ActivityManagerNative;
 import android.app.Dialog;
-import android.app.IActivityManager;
 import android.app.KeyguardManager;
 import android.app.WallpaperManager;
 import android.app.admin.DevicePolicyManager;
@@ -90,6 +88,7 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.util.EmergencyAffordanceManager;
 import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.du.Utils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.HardwareUiLayout;
@@ -441,7 +440,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_LOCKDOWN.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0, getCurrentUser().id) != 0
-                        && shouldDisplayLockdown() && !isInLockTaskMode()) {
+                        && shouldDisplayLockdown() && !Utils.isInLockTaskMode()) {
                     mItems.add(getLockdownAction());
                     mHasLockdownButton = true;
                 }
@@ -457,7 +456,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.SCREENSHOT_IN_POWER_MENU, 1, getCurrentUser().id) != 0
-                        && !isInLockTaskMode()) {
+                        && !Utils.isInLockTaskMode()) {
                     mItems.add(new ScreenshotAction());
                 }
             /*} else if (GLOBAL_ACTION_KEY_LOGOUT.equals(actionKey)) {
@@ -469,7 +468,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_ADVANCED.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.ADVANCED_REBOOT_IN_POWER_MENU, 0,
-                            getCurrentUser().id) != 0 && !isInLockTaskMode()) {
+                            getCurrentUser().id) != 0 && !Utils.isInLockTaskMode()) {
                     mItems.add(mShowAdvancedToggles);
                 }
             } else {
@@ -1341,7 +1340,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         switch (type) {
             case RESTART_HOT_BUTTON:
                 h.sendEmptyMessage(MESSAGE_DISMISS);
-                doHotReboot();
+                Utils.doHotReboot();
                 break;
             case RESTART_RECOVERY_BUTTON:
                 h.sendEmptyMessage(MESSAGE_DISMISS);
@@ -1357,7 +1356,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 won't show the LegacyGlobalActions after systemui restart
                 */
                 funcs.onGlobalActionsHidden();
-                restartSystemUI(ctx);
+                Process.killProcess(Process.myPid());
                 break;
             default:
                 break;
@@ -1759,30 +1758,6 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         public void setKeyguardShowing(boolean keyguardShowing) {
             mKeyguardShowing = keyguardShowing;
-        }
-    }
-
-    public static void restartSystemUI(Context ctx) {
-        Process.killProcess(Process.myPid());
-    }
-
-    private static void doHotReboot() {
-        try {
-            final IActivityManager am =
-                  ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
-            if (am != null) {
-                am.restart();
-            }
-        } catch (RemoteException e) {
-            Log.e(TAG, "failure trying to perform hot reboot", e);
-        }
-    }
-
-    private boolean isInLockTaskMode() {
-        try {
-            return ActivityManagerNative.getDefault().isInLockTaskMode();
-        } catch (RemoteException e) {
-            return false;
         }
     }
 }

AOSiP/platform_frameworks_base

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -22,9 +22,7 @@ import static com.android.internal.widget.LockPatternUtils.StrongAuthTracker.STR
 import static com.android.internal.widget.LockPatternUtils.StrongAuthTracker.STRONG_AUTH_REQUIRED_AFTER_USER_LOCKDOWN;
 
 import android.app.ActivityManager;
-import android.app.ActivityManagerNative;
 import android.app.Dialog;
-import android.app.IActivityManager;
 import android.app.KeyguardManager;
 import android.app.WallpaperManager;
 import android.app.admin.DevicePolicyManager;
@@ -90,6 +88,7 @@ import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.util.EmergencyAffordanceManager;
 import com.android.internal.util.ScreenshotHelper;
+import com.android.internal.util.aosip.  aosipUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.HardwareUiLayout;
@@ -441,7 +440,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_LOCKDOWN.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0, getCurrentUser().id) != 0
-                        && shouldDisplayLockdown() && !isInLockTaskMode()) {
+                        && shouldDisplayLockdown() && !  aosipUtils.isInLockTaskMode()) {
                     mItems.add(getLockdownAction());
                     mHasLockdownButton = true;
                 }
@@ -457,7 +456,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.SCREENSHOT_IN_POWER_MENU, 1, getCurrentUser().id) != 0
-                        && !isInLockTaskMode()) {
+                        && !  aosipUtils.isInLockTaskMode()) {
                     mItems.add(new ScreenshotAction());
                 }
             /*} else if (GLOBAL_ACTION_KEY_LOGOUT.equals(actionKey)) {
@@ -469,7 +468,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else if (GLOBAL_ACTION_KEY_ADVANCED.equals(actionKey)) {
                 if (Settings.Secure.getIntForUser(mContext.getContentResolver(),
                             Settings.Secure.ADVANCED_REBOOT_IN_POWER_MENU, 1,
-                            getCurrentUser().id) != 0 && !isInLockTaskMode()) {
+                            getCurrentUser().id) != 0 && !  aosipUtils.isInLockTaskMode()) {
                     mItems.add(mShowAdvancedToggles);
                 }
             } else {
@@ -1341,7 +1340,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         switch (type) {
             case RESTART_HOT_BUTTON:
                 h.sendEmptyMessage(MESSAGE_DISMISS);
-                doHotReboot();
+                  aosipUtils.doHotReboot();
                 break;
             case RESTART_RECOVERY_BUTTON:
                 h.sendEmptyMessage(MESSAGE_DISMISS);
@@ -1357,7 +1356,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 won't show the LegacyGlobalActions after systemui restart
                 */
                 funcs.onGlobalActionsHidden();
-                restartSystemUI(ctx);
+                Process.killProcess(Process.myPid());
                 break;
             default:
                 break;
@@ -1759,30 +1758,6 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
         public void setKeyguardShowing(boolean keyguardShowing) {
             mKeyguardShowing = keyguardShowing;
-        }
-    }
-
-    public static void restartSystemUI(Context ctx) {
-        Process.killProcess(Process.myPid());
-    }
-
-    private static void doHotReboot() {
-        try {
-            final IActivityManager am =
-                  ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
-            if (am != null) {
-                am.restart();
-            }
-        } catch (RemoteException e) {
-            Log.e(TAG, "failure trying to perform hot reboot", e);
-        }
-    }
-
-    private boolean isInLockTaskMode() {
-        try {
-            return ActivityManagerNative.getDefault().isInLockTaskMode();
-        } catch (RemoteException e) {
-            return false;
         }
     }
 }