f955bafc37bc8d9bb10b0b85766de804122453d0
ezio84 <brabus84@gmail.com>
2019-01-05 19:10:53 -0600

    Auto switch to a dark analog clock when using dark system theme
    - Ensure that accents change with the themes as well
    
    Change-Id: I42c4c009f6961cf1f58538ef17d9b956feb75744
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -210,6 +210,10 @@ public class KeyguardStatusView extends GridLayout implements
         updateDark();
     }
 
+    public void onThemeChanged(boolean useDarkTheme) {
+        mCustomClockView.onThemeChanged(useDarkTheme, true);
+    }
+
     /**
      * Moves clock and separator, adjusting margins when slice content changes.
      */
