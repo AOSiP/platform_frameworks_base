66401eaf9ecc4131dfd8859b97c4ade80d5f5094
Akhil Narang <akhilnarang.1999@gmail.com>
2019-01-11 15:32:05 +0100

    Revert "Blacklist apps in the ChooserActivity [1/3]"
    This reverts commit d1da689a83a06fe822702b62d81dfa6efd601974.
    
    Change-Id: Idd3a6f5aac5141e1ec7341b263674c3ea475f69a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4468,12 +4468,6 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
-         * Apps to hide in the ChooserActivity
-         * @hide
-         */
-        public static final String CHOOSER_ACTIVITY_BLACKLIST = "chooser_activity_blacklist";
-
-        /**
          * 0 - fullscreen
          * 1 - partial
          * @hide
