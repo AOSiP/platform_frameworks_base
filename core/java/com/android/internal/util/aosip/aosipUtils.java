eea90c7b2f8ed5b37844955b2bce72d457e8e191
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    Utils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc

--- core/java/com/android/internal/util/aosip/aosipUtils.java
+++ core/java/com/android/internal/util/aosip/aosipUtils.java
@@ -293,6 +293,18 @@ public class aosipUtils {
             }
         }
     }
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
     // Method to detect if device is plugged in (wired or wireless)
     public static boolean isPlugged(Context context) {
         boolean isPlugged;
