AOSiP/platform_frameworks_base

commit bc8435667eb773f2e754695dd932a8a862f4f88a

Ambient display music ticker [1/3]
Trigger an Ambient Display notification, even if Ambient
Display is disabled, so we can know playing track title/artist.
This is handy when the player starts playing a new track or even
when we use volume rocker skip track feature when screen is off.
A new track will force an ambient notification but with a clean
layout without clock and other elements, but track info in the middle
of the screen.

Double tapping on track info allows to skip the track (even for
remote Spotify/KdeConnect or other similar apps).

We use provided Doze services to skip this when the proximity
sensor is covered or system Ambient events are pending.

If the feature is disabled, track info is still shown by default
on keyguard and ambient when these modes are called by the system
or the user.

Change-Id: I1bf1ad83bce7c5a2a14824ac2993569d05356db8
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -131,6 +131,7 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
     private ExpandableNotificationRow.OnAppOpsClickListener mOnAppOpsClickListener;
 
     private NotificationData.Entry mEntryToRefresh;
+    private boolean mDontPulse;
 
     /**
      * Notifications with keys in this set are not actually around anymore. We kept them around
@@ -476,14 +477,24 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
 
         if (mEntryToRefresh == entry) {
             final Notification n = entry.notification.getNotification();
-            final int[] colors = {n.backgroundColor, n.foregroundColor,
-                    n.primaryTextColor, n.secondaryTextColor};
-            mMediaManager.setPulseColors(n.isColorizedMedia(), colors);
+            String notificationText = null;
+            final String title = n.extras.getString(Notification.EXTRA_TITLE);
+            final String text = n.extras.getString(Notification.EXTRA_TEXT);
+            if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(text)) {
+                notificationText = title + " - " + text;
+            }
+            mMediaManager.setMediaNotificationText(notificationText);
+            if (!mDontPulse) {
+                final int[] colors = {n.backgroundColor, n.foregroundColor,
+                        n.primaryTextColor, n.secondaryTextColor};
+                mMediaManager.setPulseColors(n.isColorizedMedia(), colors);
+            }
         }
     }
 
-    public void setEntryToRefresh(NotificationData.Entry entry) {
+    public void setEntryToRefresh(NotificationData.Entry entry, boolean dontPulse) {
         mEntryToRefresh = entry;
+        mDontPulse = dontPulse;
     }
 
     @Override

core/java/android/provider/Settings.java
@@ -4732,6 +4732,14 @@ public final class Settings {
          */
         public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
 
+         /**
+         * Force an Ambient notification when a new media track is being played
+         * 0 - disabled
+         * 1 - show track info within normal Ambient Display and force a new Ambient clean layout when skipping tracks
+         * @hide
+         */
+        public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

core/java/com/android/internal/hardware/AmbientDisplayConfiguration.java
@@ -40,6 +40,7 @@ public class AmbientDisplayConfiguration {
                 || pulseOnDoubleTapEnabled(user)
                 || pulseOnLongPressEnabled(user)
                 || pulseOnCustomDozeEventEnabled(user)
+                || pulseOnMedia(user)
                 || alwaysOnEnabled(user);
     }
 
@@ -60,6 +61,16 @@ public class AmbientDisplayConfiguration {
         return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
                 || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0)
                 && pulseOnNotificationAvailable();
+    }
+
+    public boolean pulseOnMedia(int user) {
+        boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 1, user) != 0;
+        return enabled && ambientDisplayAvailable();
+    }
+
+    public boolean canForceDozeNotifications() {
+        return mContext.getResources().getBoolean(R.bool.config_canForceDozeNotifications);
     }
 
     public boolean pulseOnPickupEnabled(int user) {

core/res/res/values/aosip_config.xml
@@ -82,6 +82,9 @@
     <!-- Whether device has dash charging support -->
     <bool name="config_hasDashCharger">false</bool>
 
+    <!-- Ambient display music ticker -->
+    <bool name="config_canForceDozeNotifications">true</bool>
+
 	<!-- Any package that has one of the package names defined below will be prevented
          from using specific sensors in order to stop battery drain -->
 	<string-array name="config_blockPackagesSensorDrain">

core/res/res/values/aosip_symbols.xml
@@ -45,6 +45,9 @@
     <java-symbol type="string" name="url_copy_success" />
     <java-symbol type="string" name="url_copy_failed" />
 
+    <!-- Ambient display music ticker -->
+    <java-symbol type="bool" name="config_canForceDozeNotifications" />
+
     <!-- Power Menu -->
     <java-symbol type="string" name="reboot_to_recovery_title" />
     <java-symbol type="string" name="reboot_to_recovery_message" />

packages/SystemUI/res/layout/ambient_indication_inner.xml
@@ -27,9 +27,21 @@
         android:textSize="14.0sp"
         android:textColor="?attr/wallpaperTextColorSecondary"
         android:id="@+id/ambient_indication_text"
+        android:layout_marginBottom="6.0dip"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:singleLine="true"
+        android:scrollHorizontally="true"
+        android:focusable="true"
+        android:focusableInTouchMode="true"
+        android:forceHasOverlappingRendering="false" />
+
+    <TextView
+        android:textSize="14.0sp"
+        android:textColor="?attr/wallpaperTextColorSecondary"
+        android:id="@+id/ambient_indication_track_lenght"
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"
         android:singleLine="true"
         android:forceHasOverlappingRendering="false" />
-
 </LinearLayout>

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -46,6 +46,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.ViewClippingUtil;
 import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
+import com.android.systemui.doze.DozeLog;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.util.wakelock.KeepAwakeAnimationListener;
 
@@ -78,6 +79,8 @@ public class KeyguardStatusView extends GridLayout implements
     private int mTextColor;
     private float mWidgetPadding;
     private int mLastLayoutHeight;
+
+    private boolean mForcedMediaDoze;
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
 
@@ -458,9 +461,19 @@ public class KeyguardStatusView extends GridLayout implements
         updateDozeVisibleViews();
     }
 
+    public void setCleanLayout(int reason) {
+        mForcedMediaDoze =
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
+        updateDozeVisibleViews();
+    }
+
     private void updateDozeVisibleViews() {
         for (View child : mVisibleInDoze) {
-            child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            if (!mForcedMediaDoze) {
+                child.setAlpha(mDarkAmount == 1 && mPulsing ? 0.8f : 1);
+            } else {
+                child.setAlpha(mDarkAmount == 1 ? 0 : 1);
+            }
         }
     }
 

packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -2,27 +2,44 @@ package com.android.systemui.ambientmusic;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.MediaMetadata;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.systemui.AutoReinflateContainer;
-import com.android.systemui.doze.DozeReceiver;
 import com.android.systemui.R;
+import com.android.systemui.doze.DozeLog;
 import com.android.systemui.statusbar.phone.StatusBar;
 
 import com.android.systemui.ambientmusic.AmbientIndicationInflateListener;
 
-public class AmbientIndicationContainer extends AutoReinflateContainer implements DozeReceiver {
+import java.util.concurrent.TimeUnit;
+
+public class AmbientIndicationContainer extends AutoReinflateContainer {
     private View mAmbientIndication;
-    private boolean mDozing;
     private ImageView mIcon;
     private CharSequence mIndication;
     private StatusBar mStatusBar;
     private TextView mText;
+    private TextView mTrackLenght;
     private Context mContext;
+    private MediaMetadata mMediaMetaData;
+    private String mMediaText;
+    private boolean mForcedMediaDoze;
+    private Handler mHandler;
+    private boolean mInfoAvailable;
+    private String mInfoToSet;
+    private String mLengthInfo;
+    private boolean mDozing;
+    private String mLastInfo;
 
     public AmbientIndicationContainer(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -30,36 +47,134 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
     }
 
     public void hideIndication() {
-        setIndication(null);
+        setIndication(null, null);
     }
 
-    public void initializeView(StatusBar statusBar) {
+    public void initializeView(StatusBar statusBar, Handler handler) {
         mStatusBar = statusBar;
         addInflateListener(new AmbientIndicationInflateListener(this));
+        mHandler = handler;
     }
 
     public void updateAmbientIndicationView(View view) {
         mAmbientIndication = findViewById(R.id.ambient_indication);
         mText = (TextView)findViewById(R.id.ambient_indication_text);
+        mTrackLenght = (TextView)findViewById(R.id.ambient_indication_track_lenght);
         mIcon = (ImageView)findViewById(R.id.ambient_indication_icon);
-        setIndication(mIndication);
+        setIndication(mMediaMetaData, mMediaText);
     }
 
-    @Override
     public void setDozing(boolean dozing) {
-        mDozing = dozing;
-    }
+        if (dozing == mDozing) return;
 
-    public void setIndication(CharSequence charSequence) {
-        mText.setText(charSequence);
-        mIndication = charSequence;
-        mAmbientIndication.setClickable(false);
-        boolean infoAvaillable = TextUtils.isEmpty((CharSequence)charSequence);
-        if (infoAvaillable) {
-            mAmbientIndication.setVisibility(View.INVISIBLE);
-        } else {
+        mDozing = dozing;
+        setTickerMarquee(dozing, false);
+        if (dozing && mInfoAvailable) {
+            mText.setText(mInfoToSet);
+            mLastInfo = mInfoToSet;
+            mTrackLenght.setText(mLengthInfo);
             mAmbientIndication.setVisibility(View.VISIBLE);
+            updatePosition();
+        } else {
+            setCleanLayout(-1);
+            mAmbientIndication.setVisibility(View.INVISIBLE);
+            mText.setText(null);
+            mTrackLenght.setText(null);
         }
     }
-}
 
+    private void setTickerMarquee(boolean enable, boolean extendPulseOnNewTrack) {
+        if (enable) {
+            setTickerMarquee(false, false);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mText.setEllipsize(TruncateAt.MARQUEE);
+                    mText.setMarqueeRepeatLimit(2);
+                    mText.setSelected(true);
+                    if (extendPulseOnNewTrack && mStatusBar.isPulsing()) {
+                        mStatusBar.getDozeScrimController().extendPulseForMusicTicker();
+                    }
+                }
+            }, 1600);
+        } else {
+            mText.setEllipsize(null);
+            mText.setSelected(false);
+        }
+    }
+
+    public void setOnPulseEvent(int reason, boolean pulsing) {
+        setCleanLayout(reason);
+        setTickerMarquee(pulsing,
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION);
+    }
+
+    public void setCleanLayout(int reason) {
+        mForcedMediaDoze =
+                reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
+        updatePosition();
+    }
+
+    public void updatePosition() {
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) this.getLayoutParams();
+        lp.gravity = mForcedMediaDoze ? Gravity.CENTER : Gravity.BOTTOM;
+        this.setLayoutParams(lp);
+    }
+
+    public void setIndication(MediaMetadata mediaMetaData, String notificationText) {
+        CharSequence charSequence = null;
+        if (mediaMetaData != null) {
+            CharSequence artist = mediaMetaData.getText(MediaMetadata.METADATA_KEY_ARTIST);
+            CharSequence album = mediaMetaData.getText(MediaMetadata.METADATA_KEY_ALBUM);
+            CharSequence title = mediaMetaData.getText(MediaMetadata.METADATA_KEY_TITLE);
+            long duration = mediaMetaData.getLong(MediaMetadata.METADATA_KEY_DURATION);
+            if (artist != null && album != null && title != null) {
+                /* considering we are in Ambient mode here, it's not worth it to show
+                    too many infos, so let's skip album name to keep a smaller text */
+                charSequence = artist.toString() /*+ " - " + album.toString()*/ + " - " + title.toString();
+                if (duration != 0) {
+                    mLengthInfo = String.format("%02d:%02d",
+                            TimeUnit.MILLISECONDS.toMinutes(duration),
+                            TimeUnit.MILLISECONDS.toSeconds(duration) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))).toString();
+                }
+            }
+        }
+        if (mDozing) {
+            // if we are already showing an Ambient Notification with track info,
+            // stop the current scrolling and start it delayed again for the next song
+            setTickerMarquee(true, true);
+        }
+
+        mInfoToSet = null;
+        if (!TextUtils.isEmpty(charSequence)) {
+            mInfoToSet = charSequence.toString();
+        } else if (!TextUtils.isEmpty(notificationText)) {
+            mInfoToSet = notificationText;
+            mLengthInfo = null;
+        }
+
+        mInfoAvailable = mInfoToSet != null;
+        if (mInfoAvailable) {
+            mText.setText(mInfoToSet);
+            mTrackLenght.setText(mLengthInfo);
+            mMediaMetaData = mediaMetaData;
+            mMediaText = notificationText;
+            if (mDozing) {
+                mAmbientIndication.setVisibility(View.VISIBLE);
+            }
+            boolean isAnotherTrack = mInfoAvailable
+                    && (TextUtils.isEmpty(mLastInfo) || (!TextUtils.isEmpty(mLastInfo) && !mLastInfo.equals(mInfoToSet)));
+            if (mStatusBar != null && isAnotherTrack) {
+                mStatusBar.triggerAmbientForMedia();
+            }
+            if (mDozing) {
+                mLastInfo = mInfoToSet;
+            }
+        }
+    }
+
+    public View getIndication() {
+        return mAmbientIndication;
+    }
+}

commit c84acf1c0c21e9f3bf0bdcccfe8f487c3be1f4a4

NotificationEntryManager: Fix ClassCastException
Fixes: bc8435667eb7 ("Ambient display music ticker [1/3]")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
Change-Id: I7d423ea6301ef653a24312f3e73cc8a8d6f5b885

packages/SystemUI/src/com/android/systemui/statusbar/NotificationEntryManager.java
@@ -478,8 +478,8 @@ public class NotificationEntryManager implements Dumpable, NotificationInflater.
         if (mEntryToRefresh == entry && mMediaManager.isMediaNotification(entry)) {
             final Notification n = entry.notification.getNotification();
             String notificationText = null;
-            final String title = n.extras.getString(Notification.EXTRA_TITLE);
-            final String text = n.extras.getString(Notification.EXTRA_TEXT);
+            final CharSequence title = n.extras.getCharSequence(Notification.EXTRA_TITLE);
+            final CharSequence text = n.extras.getCharSequence(Notification.EXTRA_TEXT);
             if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(text)) {
                 notificationText = title + " - " + text;
             }