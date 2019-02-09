d102ff1b5bef7f463848bdb0aa35d7cb9e84c312
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-09 18:54:05 +0530

    PhoneStatusBarTransitions: Re-add missing battery alpha transition
    In commit 311b409a589e this call was removed for erroneously breaking
    statusbar battery icon alpha transitions. So much for code review.
    
    Fixes: 311b409a589e ("SystemUI: statusbar clock date customization (1/2)")
    Reported-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: I8f9afd39f92f2650744a1c924a6d8f69e5c18b83
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -91,6 +91,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.playTogether(
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
+                    animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC),
                     animateTransitionTo(mRightClock, newAlphaBC)
