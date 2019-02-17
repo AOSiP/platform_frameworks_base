db6b56ba7382028042311b3c5b832a2fe6e3b088
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-02-17 18:16:33 +0000

    Full gestures navbar: add missing haptic feedback events
    Change-Id: I962b59e906d70a8c2b09ba3bf1fbab473e49e3b2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -267,7 +267,10 @@ public class QuickStepController implements GestureHelper {
                     int deltaX = (int) mPreviousUpEventX - (int) event.getX();
                     int deltaY = (int) mPreviousUpEventY - (int) event.getY();
                     boolean isDoubleTapReally = deltaX * deltaX + deltaY * deltaY < sDoubleTapSquare;
-                    if (isDoubleTapReally) aosipUtils.switchScreenOff(mContext);
+                    if (isDoubleTapReally) {
+                        mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        aosipUtils.switchScreenOff(mContext);
+                    }
                 } else {
                     // this is the first tap, let's go further and schedule a
                     // mDoubleTapCancelTimeout call in the action up event so after the set time
@@ -398,6 +401,7 @@ public class QuickStepController implements GestureHelper {
                         aosipUtils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                         endQuickScrub(true /* animate */);
                         mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        aosipUtils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                     } else {
                         endQuickScrub(true /* animate */);
                     }
@@ -424,6 +428,7 @@ public class QuickStepController implements GestureHelper {
             isDoubleTapPending = false;
             // it was a single tap, let's trigger the home button action
             mHandler.removeCallbacksAndMessages(null);
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             aosipUtils.sendKeycode(KeyEvent.KEYCODE_HOME, mHandler);
         }
     };
@@ -439,6 +444,7 @@ public class QuickStepController implements GestureHelper {
 
         @Override
         public void run() {
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             moveKbCursor(isRight, true);
         }
     }
