6af4a6d2679d1804f2915a1eda497138b9a637e2
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-22 13:54:15 +0530

    Revert "fwb: Forward port lockscreen quick unlock [1/2]"
    Does not work with PINs longer than 4 digits and honestly just a dodgy
    feature overall.
    
    This reverts commit 59563efb478a08d748c8c74076f054dc6837eef8.
    
    Change-Id: I0a9650315b6dfbac49c45a36476479be5ac09d72
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -17,7 +17,6 @@
 package com.android.keyguard;
 
 import android.content.Context;
-import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -32,9 +31,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import com.android.internal.widget.LockPatternUtils.RequestThrottledException;
-import com.android.keyguard.PasswordTextView.QuickUnlockListener;
 
 /**
  * Displays a PIN pad for unlocking.
@@ -55,7 +51,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
 
     private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-    private final int userId = KeyguardUpdateMonitor.getCurrentUser();
 
     public KeyguardPINView(Context context) {
         this(context, null);
@@ -129,8 +124,8 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
             });
         }
 
-        boolean scramblePin = (Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0, UserHandle.USER_CURRENT) == 1);
+        boolean scramblePin = (Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0) == 1);
 
         if (scramblePin) {
             Collections.shuffle(sNumbers);
@@ -154,19 +149,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
                 NumPadKey view = views.get(i);
                 view.setDigit(sNumbers.get(i));
             }
-        }
-
-        boolean quickUnlock = (Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0, UserHandle.USER_CURRENT) == 1);
-
-        if (quickUnlock) {
-            mPasswordEntry.setQuickUnlockListener(new QuickUnlockListener() {
-                public void onValidateQuickUnlock(String password) {
-                    validateQuickUnlock(password);
-                }
-            });
-        } else {
-            mPasswordEntry.setQuickUnlockListener(null);
         }
     }
 
@@ -230,18 +212,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     @Override
     public boolean hasOverlappingRendering() {
         return false;
-    }
-
-    private void validateQuickUnlock(String password) {
-        if (password != null) {
-            if (password.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT
-                    && kpvCheckPassword(password)) {
-                mPasswordEntry.setEnabled(false);
-                mCallback.reportUnlockAttempt(userId, true, 0);
-                mCallback.dismiss(true, userId);
-                resetPasswordText(true, true);
-            }
-        }
     }
 
     private boolean kpvCheckPassword(String password) {
