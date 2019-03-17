AICP/frameworks_base

base: Option to use volume keys to control ring volume anytime (1/2)
AICP Note: this used to be called
Option to use volume keys to control media volume anytime

With changes in P making the media volume the default stream, it makes
more sense to add ring tone volume as the option instead.

Squashed with:
Fix forcing media stream control on tablet
There's option volume_keys_control_ring_stream (Volume keys control ringtone
volume / If on, volume keys control ringtone volume. If off, volume keys
control media volume.) in Settings which is also available on tablets but it
doesn't work on these devices (I didn't find reported issue).
As I like this option and think it may be even more wanted on media devices
such as tablets, here's a fix.

PureNexus edits:
*modified to force media stream when toggled on, ring/notification stream when off

ABC edits: use existing content resolver variable
use one void for the setting getter
enabled by default

AICP edits:
 * disabled by default
 * brought up to P by @eyosen

Change-Id: I8c0c02a9205c1accdb5404183b7fda9a0eab749b

core/java/android/provider/Settings.java
@@ -4883,6 +4883,17 @@ public final class Settings {
         private static final Validator SWAP_VOLUME_BUTTONS_VALIDATOR =
                BOOLEAN_VALIDATOR;
 
+        /**
+         * Volume key controls ringtone or media sound stream
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_TONE =
+                "volume_keys_control_ring_tone";
+
+        /** @hide */
+        private static final Validator VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR =
+               BOOLEAN_VALIDATOR;
+
         /***************************
          * AICP System Settings end
          ***************************/
@@ -5010,6 +5021,7 @@ public final class Settings {
             TORCH_LONG_PRESS_POWER_GESTURE,
             TORCH_LONG_PRESS_POWER_TIMEOUT,
             SWAP_VOLUME_BUTTONS,
+            VOLUME_KEYS_CONTROL_RING_TONE,
             // AICP Settings end
         };
 
@@ -5199,6 +5211,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_GESTURE);
             PRIVATE_SETTINGS.add(TORCH_LONG_PRESS_POWER_TIMEOUT);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
+            PRIVATE_SETTINGS.add(VOLUME_KEYS_CONTROL_RING_TONE);
             // AICP Settings end
         }
 
@@ -5368,6 +5381,7 @@ public final class Settings {
             VALIDATORS.put(TORCH_LONG_PRESS_POWER_TIMEOUT,
                     TORCH_LONG_PRESS_POWER_TIMEOUT_VALIDATOR);
             VALIDATORS.put(SWAP_VOLUME_BUTTONS, SWAP_VOLUME_BUTTONS_VALIDATOR);
+            VALIDATORS.put(VOLUME_KEYS_CONTROL_RING_TONE,VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR);
             // AICP Settings end
         }
 

AOSiP/platform_frameworks_base

Change-Id: I8c03f6d8c7b2c4818a418aeb24a5dfffd28b7ce7
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

core/java/android/provider/Settings.java
@@ -4653,6 +4653,16 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Volume key controls ringtone or media sound stream
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_TONE =
+                "volume_keys_control_ring_tone";
+        /** @hide */
+        private static final Validator VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR =
+               BOOLEAN_VALIDATOR;
+
+        /**
          * Select various accents to go along with system themes
          *
          * @hide
@@ -5250,6 +5260,7 @@ public final class Settings {
             VIBRATE_ON_DISCONNECT,
             VIBRATION_ON_CHARGE_STATE_CHANGED,
             VOLUME_BUTTON_MUSIC_CONTROL,
+            VOLUME_KEYS_CONTROL_RING_TONE,
             VOLUME_KEY_CURSOR_CONTROL,
             VOLUME_ROCKER_WAKE,
             WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
@@ -5405,6 +5416,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PUBLIC_SETTINGS.add(VIBRATION_ON_CHARGE_STATE_CHANGED);
             PUBLIC_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
+            PUBLIC_SETTINGS.add(VOLUME_KEYS_CONTROL_RING_TONE);
             PUBLIC_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
             PUBLIC_SETTINGS.add(VOLUME_ROCKER_WAKE);
             PUBLIC_SETTINGS.add(WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
@@ -5636,6 +5648,7 @@ public final class Settings {
             VALIDATORS.put(VIBRATE_ON_DISCONNECT, VIBRATE_ON_DISCONNECT_VALIDATOR);
             VALIDATORS.put(VIBRATION_ON_CHARGE_STATE_CHANGED, VIBRATION_ON_CHARGE_STATE_CHANGED_VALIDATOR);
             VALIDATORS.put(VOLUME_BUTTON_MUSIC_CONTROL, VOLUME_BUTTON_MUSIC_CONTROL_VALIDATOR);
+            VALIDATORS.put(VOLUME_KEYS_CONTROL_RING_TONE,VOLUME_KEYS_CONTROL_RING_TONE_VALIDATOR);
             VALIDATORS.put(VOLUME_KEY_CURSOR_CONTROL, VOLUME_KEY_CURSOR_CONTROL_VALIDATOR);
             VALIDATORS.put(VOLUME_ROCKER_WAKE, VOLUME_ROCKER_WAKE_VALIDATOR);
             VALIDATORS.put(WAKE_WHEN_PLUGGED_OR_UNPLUGGED, WAKE_WHEN_PLUGGED_OR_UNPLUGGED_VALIDATOR);