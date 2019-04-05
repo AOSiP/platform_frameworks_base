e59c5551ae36957eeb0ccde663c04a16690a48ad
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    Change-Id: I52beaf6492b9daa012b5d42b858d552d1e378a2c

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -102,6 +102,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_TOGGLE_NAVIGATION_EDITOR      = 52 << MSG_SHIFT;
     private static final int MSG_DISPATCH_NAVIGATION_EDITOR_RESULTS = 53 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH_STATE     = 54 << MSG_SHIFT;
+    private static final int MSG_IN_DISPLAY_FINGERPRINT        = 55 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SETTINGS_PANEL                  = 100 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
@@ -184,6 +185,7 @@ public class CommandQueue extends IStatusBar.Stub {
         default void toggleNavigationEditor() {}
         default void dispatchNavigationEditorResults(Intent intent) {}
         default void toggleCameraFlashState(boolean enable) { }
+        default void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) { }
     }
 
     @VisibleForTesting
@@ -637,6 +639,17 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    @Override
+    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
+        synchronized (mLock) {
+            SomeArgs args = SomeArgs.obtain();
+            args.arg1 = show;
+            args.arg2 = isEnrolling;
+            mHandler.obtainMessage(MSG_IN_DISPLAY_FINGERPRINT, args)
+                    .sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -927,6 +940,13 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_CAMERA_FLASH_STATE:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlashState(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_IN_DISPLAY_FINGERPRINT:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).handleInDisplayFingerprintView(
+                                (boolean)((SomeArgs)msg.obj).arg1,
+                                (boolean)((SomeArgs)msg.obj).arg2);
                     }
                     break;
             }
