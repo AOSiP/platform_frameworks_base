292a658ab2113a403dd395d48397124567d2aad1
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-04-08 21:00:14 -0400

    Remove curent clock
    Revert "Clock: Fix crash in formatted string with date on right"
    This reverts commit e54b5f6b0bfe31dca41935b2c6cdc09495af579c.
    Revert "StatusbarClock: date right/left position [1/2]"
    This reverts commit c5cbf97bee3342cc36d75f694c3c52af6fbb93cb.
    Revert "SystemUI: statusbar clock date customization (1/2)"
    This reverts commit 311b409a589e3e485b99ba458364d3b1dcd0c502.
    
    Change-Id: I6d88b00bff01345256e39d4c9319d16e30dfb09e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -16,12 +16,8 @@
 
 package com.android.systemui.statusbar.phone;
 
-import android.content.ContentResolver;
-import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.view.DisplayCutout;
 import android.view.View;
 import android.view.WindowInsets;
@@ -230,8 +226,6 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
     }
 
     private void setShown(boolean isShown) {
-        final int clockStyle = Settings.System.getIntForUser(mClockView.getContext().getContentResolver(),
-                Settings.System.STATUSBAR_CLOCK_STYLE, 0, UserHandle.USER_CURRENT);
         if (mShown != isShown) {
             mShown = isShown;
             if (isShown) {
@@ -241,12 +235,8 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 CrossFadeHelper.fadeOut(mClockView, CONTENT_FADE_DURATION/* duration */,
                         0 /* delay */, () -> mClockView.setVisibility(View.INVISIBLE));
             } else {
-                if (clockStyle == 0) {
-                    CrossFadeHelper.fadeIn(mClockView, CONTENT_FADE_DURATION /* duration */,
-                            CONTENT_FADE_DELAY /* delay */);
-                } else {
-                    mClockView.setVisibility(View.GONE);
-                }
+                CrossFadeHelper.fadeIn(mClockView, CONTENT_FADE_DURATION /* duration */,
+                        CONTENT_FADE_DELAY /* delay */);
                 CrossFadeHelper.fadeOut(mHeadsUpStatusBarView, CONTENT_FADE_DURATION/* duration */,
                         0 /* delay */, () -> mHeadsUpStatusBarView.setVisibility(View.GONE));
 
