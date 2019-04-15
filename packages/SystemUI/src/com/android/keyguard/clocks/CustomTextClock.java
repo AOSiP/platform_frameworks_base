1d8b4833714117d2bef0f54fb9d518988e044863
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-04-15 23:20:51 +0000

    Q Clock - Unbreak tens plus units combo for EN
    Borked by 3932a5d6096ca7bb04c5a8a3f82a1c9f0051d1f8
    
    Change-Id: Ie7bd6c03c2cfe43e8f918ef3332ee1307a8a9370
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
@@ -213,7 +213,7 @@ public class CustomTextClock extends TextView {
                              NumString = TensString[tens] + UnitsString[units].toLowerCase();
                     }
                 } else {
-                    NumString = TensString[tens]+" "+UnitsString[units];
+                    NumString = TensString[tens]+" "+UnitsString[units].substring(2, UnitsString[units].length());
                 }
             }
         } else if (num < 20 ) {
@@ -251,7 +251,7 @@ public class CustomTextClock extends TextView {
                              NumString = TensString[tens] + UnitsString[units].toLowerCase();
                     }
                 } else {
-                    NumString = TensString[tens]+" "+UnitsString[units];
+                    NumString = TensString[tens]+" "+UnitsString[units].substring(2, UnitsString[units].length());
                 }
             }
         } else if (num < 10 ) {
