2a8c6e7d01f5b4f6f6609e448671f3358b1362a5
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-04-23 08:45:38 -0400

    base:Sammy Clocks: reduce padding between top and bottom row
    Change-Id: I1ab69ea90e5022933676c8bd844ffc05eee5f6f9
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -455,7 +455,7 @@ public class KeyguardStatusView extends GridLayout implements
                        View.GONE) : View.VISIBLE);
                 mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
-                mClockView.setLineSpacing(0,1f);
+                mClockView.setLineSpacing(0,0.8f);
                 mCustomClockView.setVisibility(View.GONE);
                 break;
             case 4: // sammy (bold)
@@ -463,7 +463,7 @@ public class KeyguardStatusView extends GridLayout implements
                        View.GONE) : View.VISIBLE);
                 mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(R.dimen.widget_big_font_size));
-                mClockView.setLineSpacing(0,1f);
+                mClockView.setLineSpacing(0,0.8f);
                 mCustomClockView.setVisibility(View.GONE);
                 break;
             case 5: // sammy accent
@@ -471,7 +471,7 @@ public class KeyguardStatusView extends GridLayout implements
                        View.GONE) : View.VISIBLE);
                 mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(R.dimen.widget_sammy_accent_font_size));
-                mClockView.setLineSpacing(0,1f);
+                mClockView.setLineSpacing(0,0.8f);
                 mCustomClockView.setVisibility(View.GONE);
                 break;
             case 6: // sammy accent alt
@@ -479,7 +479,7 @@ public class KeyguardStatusView extends GridLayout implements
                        View.GONE) : View.VISIBLE);
                 mClockView.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                 getResources().getDimensionPixelSize(R.dimen.widget_sammy_accent_font_size));
-                mClockView.setLineSpacing(0,1f);
+                mClockView.setLineSpacing(0,0.8f);
                 mCustomClockView.setVisibility(View.GONE);
                 break;
         }
