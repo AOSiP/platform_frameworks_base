10a0e7300c824378ea5d0f7dd195751c21fc903d
Amin Shaikh <ashaikh@google.com>
2019-04-29 07:36:34 -0400

    Update SignalDrawable.
    - Change SignalDrawable to render an SVG for the entire signal icon and
    only draw by hand the X and the carrier change animation
    - Changed carrier change animation to display a single dot at a time
    instead of cutting out the entire row
    - Removed unneeded methods due to single tone mobile signal icon
    - Remove unused airplane mode state
    - Remove unused CellTileView
    - Add demo mode command inflate to inflate signal strengths
    - Add GM2 cell signal drawables to the framework for 0-4 and 0-5 bars
    
    TODO: should empty state be the same as 0 bar state?
    
    Bug: 124107387
    Test: visual
    Change-Id: Ib5a0c3d89d35c1e21efb67e526c508aab100f2a0
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -352,10 +352,6 @@ public class MobileSignalController extends SignalController<
 
     @Override
     public int getQsCurrentIconId() {
-        if (mCurrentState.airplaneMode) {
-            return SignalDrawable.getAirplaneModeState(getNumLevels());
-        }
-
         return getCurrentIconId();
     }
 
