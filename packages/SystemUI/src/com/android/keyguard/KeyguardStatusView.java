b1994964ee5f4949b5b761f58805626b0ec422b7
paphonb <paphonb@gmail.com>
2019-05-07 13:50:36 +0000

    Q Always-On Display: Add double tap media slice to skip song
    Change-Id: Ic2776cc57f4b4e36d61a480fa45b241d14bf3b46

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -669,6 +669,10 @@ public class KeyguardStatusView extends GridLayout implements
         }
     }
 
+    public KeyguardSliceView getSliceView() {
+        return mKeyguardSlice;
+    }
+
     private class ClipChildrenAnimationListener extends AnimatorListenerAdapter implements
             ViewClippingUtil.ClippingParameters {
 
