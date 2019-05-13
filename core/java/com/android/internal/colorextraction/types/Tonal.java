2a995dd74ccbe935a152f898e774c7228e1d74b0
Harsh Shandilya <harsh@prjkt.io>
2019-05-13 15:20:32 +0530

    [WIP] TypographicClock: Tint top row with wallpaper primary color
    Change-Id: Ia4cf09bdef39fc95a910c9aea2c22f043fcd7d7d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- core/java/com/android/internal/colorextraction/types/Tonal.java
+++ core/java/com/android/internal/colorextraction/types/Tonal.java
@@ -175,6 +175,7 @@ public class Tonal implements ExtractionType {
                 Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
         float[] s = fit(palette.s, hsl[1], fitIndex, 0.0f, 1.0f);
         float[] l = fit(palette.l, hsl[2], fitIndex, 0.0f, 1.0f);
+        int[] colorPalette = getColorPalette(h, s, l);
 
         if (DEBUG) {
             StringBuilder builder = new StringBuilder("Tonal Palette - index: " + fitIndex +
@@ -211,6 +212,7 @@ public class Tonal implements ExtractionType {
         // Normal colors:
         outColorsNormal.setMainColor(mainColor);
         outColorsNormal.setSecondaryColor(mainColor);
+        outColorsNormal.setColorPalette(colorPalette);
 
         // Dark colors:
         // Stops at 4th color, only lighter if dark text is supported
@@ -224,6 +226,7 @@ public class Tonal implements ExtractionType {
         mainColor = getColorInt(primaryIndex, h, s, l);
         outColorsDark.setMainColor(mainColor);
         outColorsDark.setSecondaryColor(mainColor);
+        outColorsDark.setColorPalette(colorPalette);
 
         // Extra Dark:
         // Stay close to dark colors until dark text is supported
@@ -237,6 +240,7 @@ public class Tonal implements ExtractionType {
         mainColor = getColorInt(primaryIndex, h, s, l);
         outColorsExtraDark.setMainColor(mainColor);
         outColorsExtraDark.setSecondaryColor(mainColor);
+        outColorsExtraDark.setColorPalette(colorPalette);
 
         outColorsNormal.setSupportsDarkText(supportsDarkText);
         outColorsDark.setSupportsDarkText(supportsDarkText);
@@ -248,6 +252,14 @@ public class Tonal implements ExtractionType {
         }
 
         return true;
+    }
+
+    private int[] getColorPalette(float[] h, float[] s, float[] l) {
+        int[] palette = new int[h.length];
+        for (int i = 0; i < palette.length; i++) {
+            palette[i] = getColorInt(i, h, s, l);
+        }
+        return palette;
     }
 
     private void applyFallback(@Nullable WallpaperColors inWallpaperColors,
