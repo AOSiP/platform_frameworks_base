8a73520197ef472ba9aaa5586ded74923aa7614c
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-06-08 23:50:19 -0400

    base: Pebbles Removal(2/4)
    Revert "base: accents: add Q Green accent"
    This reverts commit 923846db0adc7213f022d7f28413a82bc8037887.
    
    Revert "base: Accents: Sweet Q Purple [1/3]"
    This reverts commit 9597cfbe1e88ed8f5a5f16ffcc022d3b2ea7e7fd.
    
    Revert "Fruity Pebbles v2.0"
    This reverts commit 29385973af23805f20115d39287b04ce9464422e.
    
    Revert "Fruity Pebbles"
    This reverts commit d963d2c61d430246f401f63a5854f864d3616341.
    
    Change-Id: Idf3070f6995fc92d2166479c7efd618e6a847fd4
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/com/android/internal/statusbar/ThemeAccentUtils.java
+++ core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -67,41 +67,6 @@ public class ThemeAccentUtils {
         "com.android.wellbeing.theme.blackaf", // 7
     };
 
-    // Accents
-    private static final String[] ACCENTS = {
-        "default_accent", // 0
-        "com.accents.red", // 1
-        "com.accents.pink", // 2
-        "com.accents.purple", // 3
-        "com.accents.deeppurple", // 4
-        "com.accents.indigo", // 5
-        "com.accents.blue", // 6
-        "com.accents.lightblue", // 7
-        "com.accents.cyan", // 8
-        "com.accents.teal", // 9
-        "com.accents.green", // 10
-        "com.accents.lightgreen", // 11
-        "com.accents.lime", // 12
-        "com.accents.yellow", // 13
-        "com.accents.amber", // 14
-        "com.accents.orange", // 15
-        "com.accents.deeporange", // 16
-        "com.accents.brown", // 17
-        "com.accents.grey", // 18
-        "com.accents.bluegrey", // 19
-        "com.accents.black", // 20
-        "com.accents.white", // 21
-        "com.accents.userone", // 22
-        "com.accents.usertwo", // 23
-        "com.accents.userthree", // 24
-        "com.accents.userfour", // 25
-        "com.accents.userfive", // 26
-        "com.accents.usersix", // 27
-        "com.accents.userseven", // 28
-        "com.accents.usereight", // 29
-        "com.accents.usernine", // 30
-    };
-
     private static final String[] QS_TILE_THEMES = {
         "default_qstile", // 0
         "com.android.systemui.qstile.squircle", // 1
@@ -173,7 +138,6 @@ public class ThemeAccentUtils {
             } catch (RemoteException e) {
             }
         }
-        unfuckBlackWhiteAccent(om, userId);
     }
 
     // Set black theme
@@ -182,96 +146,10 @@ public class ThemeAccentUtils {
             try {
                 om.setEnabled(theme,
                         useBlackAFTheme, userId);
-                unfuckBlackWhiteAccent(om, userId);
                 if (useBlackAFTheme) {
                     unloadStockDarkTheme(om, userId);
                 }
             } catch (RemoteException e) {
-            }
-        }
-    }
-
-    // Check for black and white accent overlays
-    public static void unfuckBlackWhiteAccent(IOverlayManager om, int userId) {
-        OverlayInfo themeInfo = null;
-        try {
-            if (isUsingDarkTheme (om, userId) || isUsingBlackAFTheme (om, userId)) {
-                themeInfo = om.getOverlayInfo(ACCENTS[20],
-                        userId);
-                if (themeInfo != null && themeInfo.isEnabled()) {
-                    om.setEnabled(ACCENTS[20],
-                            false /*disable*/, userId);
-                    om.setEnabled(ACCENTS[21],
-                            true, userId);
-                }
-            } else {
-                themeInfo = om.getOverlayInfo(ACCENTS[21],
-                        userId);
-                if (themeInfo != null && themeInfo.isEnabled()) {
-                    om.setEnabled(ACCENTS[21],
-                            false /*disable*/, userId);
-                    om.setEnabled(ACCENTS[20],
-                            true, userId);
-                }
-            }
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Check for any accent overlay
-    public static boolean isUsingAccent(IOverlayManager om, int userId, int accent) {
-        OverlayInfo themeInfo = null;
-        try {
-            themeInfo = om.getOverlayInfo(ACCENTS[accent],
-                    userId);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return themeInfo != null && themeInfo.isEnabled();
-    }
-
-    // Switches theme accent from one to another or back to stock
-    public static void updateAccents(IOverlayManager om, int userId, int accentSetting) {
-        if (accentSetting == 0) {
-            unloadAccents(om, userId);
-        } else if (accentSetting < 20) {
-            try {
-                om.setEnabled(ACCENTS[accentSetting],
-                        true, userId);
-            } catch (RemoteException e) {
-            }
-        } else if (accentSetting > 21) {
-            try {
-                om.setEnabled(ACCENTS[accentSetting],
-                        true, userId);
-            } catch (RemoteException e) {
-            }
-        } else if (accentSetting == 20) {
-            try {
-                // If using a dark theme we use the white accent, otherwise use the black accent
-                if (isUsingDarkTheme(om, userId) || isUsingBlackAFTheme(om, userId)) {
-                    om.setEnabled(ACCENTS[21],
-                            true, userId);
-                } else {
-                    om.setEnabled(ACCENTS[20],
-                            true, userId);
-                }
-            } catch (RemoteException e) {
-            }
-        }
-    }
-
-    // Unload all the theme accents
-    public static void unloadAccents(IOverlayManager om, int userId) {
-        // skip index 0
-        for (int i = 1; i < ACCENTS.length; i++) {
-            String accent = ACCENTS[i];
-            try {
-                om.setEnabled(accent,
-                        false /*disable*/, userId);
-            } catch (RemoteException e) {
-                e.printStackTrace();
             }
         }
     }
