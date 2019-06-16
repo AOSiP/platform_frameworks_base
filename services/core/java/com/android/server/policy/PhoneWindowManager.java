e86cb8a12257144f24057e0b804b1d48e5d80cbb
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-06-16 23:28:41 +0530

    PhoneWindowManager: Set delay for screenshot shortcut to 0
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I2e177f91b03155ec016d1c9acc87938da3f7f551

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -804,7 +804,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // Screenshot trigger states
     // Time to volume and power must be pressed within this interval of each other.
-    private static final long SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS = 150;
+    private static final long SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS = 0;
     // Increase the chord delay when taking a screenshot from the keyguard
     private static final float KEYGUARD_SCREENSHOT_CHORD_DELAY_MULTIPLIER = 2.5f;
     private boolean mScreenshotChordEnabled;
