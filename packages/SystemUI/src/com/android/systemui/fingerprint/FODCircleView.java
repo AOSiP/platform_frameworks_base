847138c66b51968102e187eb8c24b5995d95c67a
Peter Cai <peter@typeblog.net>
2019-06-20 15:03:06 +0800

    FODCircleView: read from system props
    * Match the latest phh implementation
    
    Change-Id: I6fcd8a23748679321e02e0cb1cd215a61f6051cb
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
@@ -64,10 +64,6 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
     private final WindowManager mWM;
 
-    private final int mCircleX = 595;
-    private final int mCircleY = 2527;
-    private final int mCircleSize = 250;
-
     private boolean mIsDreaming;
     private boolean mIsPulsing;
     private boolean mIsScreenOn;
@@ -171,12 +167,21 @@ public class FODCircleView extends ImageView implements OnTouchListener {
     FODCircleView(Context context) {
         super(context);
 
-        mX = mCircleX;
-        mY = mCircleY;
-        mW = mCircleSize;
-        mH = mCircleSize;
+        String[] location = android.os.SystemProperties.get("persist.vendor.sys.fp.fod.location.X_Y", "").split(",");
+        String[] size = android.os.SystemProperties.get("persist.vendor.sys.fp.fod.size.width_height", "").split(",");
+        if(size.length == 2 && location.length == 2) {
+            mX = Integer.parseInt(location[0]);
+            mY = Integer.parseInt(location[1]);
+            mW = Integer.parseInt(size[0]);
+            mH = Integer.parseInt(size[1]);
+        } else {
+            mX = -1;
+            mY = -1;
+            mW = -1;
+            mH = -1;
+        }
 
-        mPaintFingerprint.setAntiAlias(true);
+	mPaintFingerprint.setAntiAlias(true);
         mPaintFingerprint.setColor(Color.GREEN);
 
         setImageResource(R.drawable.fod_icon_default);
