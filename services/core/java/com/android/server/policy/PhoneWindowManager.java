0441b56b2a91da14c343a4b7904794852aa36265
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-06 02:33:42 -0300

    PocketLock: Remove animations
    Causing flickering issues
    
    Change-Id: Ie92c629e14004db30a967ca9b0eb32890099249f

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1483,7 +1483,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mPowerKeyHandled = true;
                 performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
                         "Power - Long Press - Hide Pocket Lock");
-                hidePocketLock(true);
+                hidePocketLock();
                 mPocketManager.setListeningExternal(false);
                 break;
         }
@@ -5079,9 +5079,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void handleDevicePocketStateChanged() {
         final boolean interactive = mPowerManager.isInteractive();
         if (mIsDeviceInPocket) {
-            showPocketLock(interactive);
+            showPocketLock();
         } else {
-            hidePocketLock(interactive);
+            hidePocketLock();
         }
     }
 
@@ -5092,7 +5092,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * @see this.mPocketCallback;
      * @author Carlo Savignano
      */
-    private void showPocketLock(boolean animate) {
+    private void showPocketLock() {
         if (!mSystemReady || !mSystemBooted || !mKeyguardDrawnOnce
                 || mPocketLock == null || mPocketLockShowing) {
             return;
@@ -5103,10 +5103,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (DEBUG_INPUT) {
-            Log.d(TAG, "showPocketLock, animate=" + animate);
+            Log.d(TAG, "showPocketLock");
         }
 
-        mPocketLock.show(animate);
+        mPocketLock.show();
         mPocketLockShowing = true;
 
         mPocketManager.setPocketLockVisible(true);
@@ -5119,17 +5119,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * @see this.mPocketCallback;
      * @author Carlo Savignano
      */
-    private void hidePocketLock(boolean animate) {
+    private void hidePocketLock() {
         if (!mSystemReady || !mSystemBooted || !mKeyguardDrawnOnce
                 || mPocketLock == null || !mPocketLockShowing) {
             return;
         }
 
         if (DEBUG_INPUT) {
-            Log.d(TAG, "hidePocketLock, animate=" + animate);
+            Log.d(TAG, "hidePocketLock");
         }
 
-        mPocketLock.hide(animate);
+        mPocketLock.hide();
         mPocketLockShowing = false;
 
         mPocketManager.setPocketLockVisible(false);
