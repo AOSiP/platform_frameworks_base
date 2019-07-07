b959be4988ab0ab547b8f74566a7fcb1b9ab9aa5
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 12:11:43 +0530

    Disable long click for SoundSearch tile
    Change-Id: Id893722185fa4f111f2730849d1c8abfffa121dc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,7 +44,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.SoundSearchTIle;
+import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -83,7 +83,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
     private final Provider<AdbOverNetworkTile> mAdbOverNetworkProvider;
-    private final Provider<SoundSearchTIle> mSoundSearchTIleProvider;
+    private final Provider<SoundSearchTile> mSoundSearchTileProvider;
 
     private QSTileHost mHost;
 
@@ -110,7 +110,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<AODTile> aodTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
             Provider<AdbOverNetworkTile> adbOverNetworkProvider,
-            Provider<SoundSearchTIle> soundSearchTIleProvider) {
+            Provider<SoundSearchTile> soundSearchTIleProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -134,7 +134,7 @@ public class QSFactoryImpl implements QSFactory {
         mAODTileProvider = aodTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
         mAdbOverNetworkProvider = adbOverNetworkProvider;
-        mSoundSearchTIleProvider = soundSearchTIleProvider;
+        mSoundSearchTileProvider = soundSearchTIleProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -196,7 +196,7 @@ public class QSFactoryImpl implements QSFactory {
             case "adb_network":
                 return mAdbOverNetworkProvider.get();
             case "soundsearch":
-                return mSoundSearchTIleProvider.get();
+                return mSoundSearchTileProvider.get();
         }
 
         // Intent tiles.
