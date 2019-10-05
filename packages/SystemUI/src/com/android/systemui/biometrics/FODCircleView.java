d9d721b0c1fe9a3c16ed6912efff60c8aaf460c6
Abhishek Aggarwal <warabhishek@gmail.com>
2019-10-05 18:41:45 +0300

    raphael: miui fp icon
    Change-Id: I9d908e8940c9168c318ea16b6ab35807b6b4321c

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -98,7 +98,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
             mHandler.post(() -> {
                 setDim(true);
-                setImageDrawable(null);
+                setImageResource(R.drawable.fod_icon_pressed);
 
                 invalidate();
             });
@@ -260,7 +260,8 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         super.onDraw(canvas);
 
         if (mIsInsideCircle) {
-            canvas.drawCircle(mWidth / 2, mHeight / 2, (float) (mWidth / 2.0f), mPaintFingerprint);
+            //canvas.drawCircle(mWidth / 2, mHeight / 2, (float) (mWidth / 2.0f), mPaintFingerprint);
+            setImageResource(R.drawable.fod_icon_pressed);
         }
     }
 
@@ -336,7 +337,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             setDim(true);
-            setImageDrawable(null);
+            setImageResource(R.drawable.fod_icon_pressed);
         }
 
         return true;
