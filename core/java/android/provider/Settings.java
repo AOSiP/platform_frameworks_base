3d38b4674f216a39da31f11fd8c52823b0140adc
alexxxdev <alexxxdev@yandex.ru>
2019-10-17 22:24:24 +0100

    Extended Audio Panel [1/2]
    Change-Id: Ieb85137ee73f7ed032b9bbdd4087be2c72a4d1f5
    Signed-off-by: a.derendyaev <a.derendyaev@magdv.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5271,6 +5271,24 @@ public final class Settings {
         /** @hide */
         private static final Validator FORCE_FULLSCREEN_CUTOUT_APPS_VALIDATOR = ANY_STRING_VALIDATOR;
 
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
+
+        /** @hide */
+        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
+
         /**
          * Wheter to show network traffic indicator in statusbar
          * @hide
