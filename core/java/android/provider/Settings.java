533ab6b36b8d38e9caf7501e58e831957a3980b5
Mazda-- <alex@dirtyunicorns.com>
2019-10-22 17:19:42 +0530

    Lockscreen Clock Widget Fonts [1/2]
    SuperDroidBond-:
    
    - Forward Port to Q.
    - Add all new fonts for LS.
    - Move Font from KeyguardStatusView to KeyguardClockSwitch
    - Now we got 30+ options to choose from
    
    This is beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    BenzoEdit:
    Fixed the fonts being tiny
    
    Evolution X:
    - Set Google Sans as default font style
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5692,6 +5692,13 @@ public final class Settings {
                BOOLEAN_VALIDATOR;
 
         /**
+         * Change fonts for the system lockscreen clock widget
+         *
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
