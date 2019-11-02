f5bb6d4cddee634c298ff37f731b790ee3a0f71f
ezio84 <brabus84@gmail.com>
2019-11-02 10:23:25 +0100

    Allow to choose lockscreen or Ambient on wake gestures [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    PS: we could merge the "else if (isWakeLockScreen)" case in the
    longpress/wakegestures one considering it also triggers
    a requestPulse, but let's keep it as it's now just to avoid
    future merge conflicts there
    
    Change-Id: Ie373dee62bd55d4d88a74fdfa1701df575144705
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -168,12 +168,13 @@ public class DozeTriggers implements DozeMachine.Part {
 
         if (isWakeDisplay) {
             onWakeScreen(wakeEvent, mMachine.isExecutingTransition() ? null : mMachine.getState());
-        } else if (isLongPress) {
-            requestPulse(pulseReason, true /* alreadyPerformedProxCheck */,
-                    null /* onPulseSupressedListener */);
+        } else if (isLongPress
+                // whether to show ambient or lockscreen if AoD is disabled and we do a wake gesture like lift to wake or double tap
+               || (!mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT) && mConfig.isAmbientGestureEnabled(UserHandle.USER_CURRENT))) {
+            requestPulse(pulseReason, true, null /* onPulseSupressedListener */);
         } else if (isWakeLockScreen) {
             if (wakeEvent) {
-                requestPulse(pulseReason, true /* alreadyPerformedProxCheck */,
+                requestPulse(pulseReason, true,
                         null /* onPulseSupressedListener */);
             }
         } else {
@@ -186,7 +187,7 @@ public class DozeTriggers implements DozeMachine.Part {
                     if (screenX != -1 && screenY != -1) {
                         mDozeHost.onSlpiTap(screenX, screenY);
                     }
-                    gentleWakeUp(pulseReason);
+                    //gentleWakeUp(pulseReason);
                 } else if (isPickup) {
                     gentleWakeUp(pulseReason);
                 } else {
@@ -577,6 +578,11 @@ public class DozeTriggers implements DozeMachine.Part {
             if (active) {
                 mMachine.requestState(DozeMachine.State.FINISH);
             }
+        }
+
+        @Override
+        public void wakeUpFromDoubleTap() {
+            gentleWakeUp(DozeLog.REASON_SENSOR_DOUBLE_TAP);
         }
     };
 }
