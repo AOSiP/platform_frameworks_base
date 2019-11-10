f719e4b189ae295bde64997b9fa65f5659d41c66
ZeNiXxX <zenixxx.havoc@gmail.com>
2019-11-10 15:26:58 +0100

    base: Add GoogleSans font overlay
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -35,6 +35,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontCagliostroSourceOverlay \
 	FontCoolstorySourceOverlay \
 	FontLGSmartGothicSourceOverlay \
+	FontGoogleSansOverlay \
 	FontNotoSerifSourceOverlay \
 	FontRosemarySourceOverlay \
 	FontSonySketchSourceOverlay \
