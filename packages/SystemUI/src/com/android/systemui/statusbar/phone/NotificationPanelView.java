baa95fac944542cc40e24dfb8f13fa206aefdbeb
maxwen <max.weninger@gmail.com>
2019-11-18 00:29:15 +0100

    SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    -Fixes the issue where you can only wake from aod once using dt2w
    Change-Id: I8ca091471e78bf1624935c7ea7444e93fc89a8ce
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1292,7 +1292,7 @@ public class NotificationPanelView extends PanelView implements
         if (mStatusBar.isBouncerShowingScrimmed()) {
             return false;
 	}
-        if (!mQsExpanded
+        if (!mQsExpanded && !mPulsing && !mDozing
                 && mDoubleTapToSleepEnabled
                 && event.getY() < mStatusBarHeaderHeight) {
             if (mDoubleTapGesture.onTouchEvent(event)) {
