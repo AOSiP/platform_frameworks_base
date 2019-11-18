2e82c7cf0c741b4db2f70443d69ed98a12a66a51
ezio84 <brabus84@gmail.com>
2019-11-18 22:56:47 +0100

    Losckreen blur level: code fixes
    check if blur_radius < 1 before checking if it's <5
    
    use existing statusbar settings observer to avoid too many settings
    provider calls when triggering processArtwork (it's called also on
    wake events when goint to lockscreen)
    
    set the default system setting value to 2500,
    because then we divide it for 100 to get the stock default value
    that was 25 in MediaArtwork class.
    Remember to set android:defaultValue to 2500 for the seekbar in
    the Settings part of this commit.
    
    Change-Id: I63cbebe8c0956362190496a3628c80324408f78f
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1772,6 +1772,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -1779,11 +1782,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (uri.equals(Settings.Secure.getUriFor(
                     Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS))) {
                 setFpToDismissNotifications();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
+                setLockScreenMediaBlurLevel();
             }
         }
 
         public void update() {
             setFpToDismissNotifications();
+            setLockScreenMediaBlurLevel();
         }
     }
 
@@ -1791,6 +1798,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         mFpDismissNotifications = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS, 0,
                 UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setLockScreenMediaBlurLevel() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaBlurLevel();
+        }
     }
 
     /**
