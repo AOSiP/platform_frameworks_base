235e34ba97b433e6fc0341412f9e01e7f24bb3ba
ezio84 <brabus84@gmail.com>
2019-12-08 20:41:34 +0100

    Custom doze: remove custom brightness for now (see commit msg)
    on Q, aod brightness value is set to the min value between the
    value given by the sensor/array and the user actual brightness,
    so the feature is pretty useless the way it's now
    https://github.com/ezio84/abc_frameworks_base/commit/c48b74af00925758ed892fb18717fa5d56da38a7#diff-2298f293d0638ded26bf7b689560c650
    
    Change-Id: I8dc6783727a9edead330d25c79205fd10c77f23b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4919,13 +4919,6 @@ public final class Settings {
         public static final String CUSTOM_AMBIENT_POCKETMODE_GESTURE = "custom_ambient_pocketmode_gesture";
 
         /**
-         * Ambient screen settings
-         * "screen_brightness_array=0:1:2:3:4"
-         * @hide
-         */
-        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
-
-        /**
          * Whether to show Brightness Icon On Brightness Slider
          * @hide
          */
