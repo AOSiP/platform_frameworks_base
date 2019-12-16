f3b5d492e2bf74b2732bdfd2d626047a0f475edb
Stallix <team.evolutionx.rom@gmail.com>
2019-12-16 04:20:53 +0000

    SystemUI: Add VoLIT icon [1/2]
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Change-Id: Ied1a4e5f985e1615dff3539feb0e3cc2551768cb

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -430,7 +430,11 @@ public class MobileSignalController extends SignalController<
                 case 3:
                     resId = R.drawable.ic_hd_volte;
                     break;
- 	        //Vo
+		  // VoLIT
+                case 4:
+                    resId = R.drawable.ic_volit;
+                    break;
+		 //Vo
                 case 0:
                 default:
                     resId = R.drawable.ic_volte;
