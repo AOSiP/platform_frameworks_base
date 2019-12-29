5ac1ff080890a487a5ab40accb0c1fee3897daec
NurKeinNeid <mralexman3000@gmail.com>
2019-12-29 17:00:06 +0100

    Revert "KeyguardSliceView: adjust placement when Type clock is in use"
    This reverts commit 0d88cecbd0bdeeb4d4c4a87c652acfdcdd06b05b.
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -24,7 +24,6 @@ import android.transition.TransitionValues;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.MathUtils;
-import android.view.Gravity;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,7 +36,6 @@ import androidx.annotation.VisibleForTesting;
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.internal.colorextraction.ColorExtractor.OnColorsChangedListener;
 import com.android.keyguard.clock.ClockManager;
-import com.android.keyguard.KeyguardSliceView;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
@@ -121,7 +119,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
      * Status area (date and other stuff) shown below the clock. Plugin can decide whether or not to
      * show it below the alternate clock.
      */
-    private KeyguardSliceView mKeyguardStatusArea;
+    private View mKeyguardStatusArea;
 
     /**
      * Maintain state so that a newly connected plugin can be initialized.
@@ -249,7 +247,6 @@ public class KeyguardClockSwitch extends RelativeLayout {
             mClockPlugin.onDestroyView();
             mClockPlugin = null;
         }
-        adjustStatusAreaPadding(plugin);
         if (plugin == null) {
             if (mShowingHeader) {
                 mClockView.setVisibility(View.GONE);
@@ -458,14 +455,6 @@ public class KeyguardClockSwitch extends RelativeLayout {
                 mBigClockContainer.setVisibility(VISIBLE);
             }
         }
-    }
-
-    private void adjustStatusAreaPadding(ClockPlugin plugin) {
-        final boolean mIsTypeClock = plugin != null && plugin.getName().equals("type");
-        mKeyguardStatusArea.setRowGravity(mIsTypeClock ? Gravity.LEFT : Gravity.CENTER);
-        mKeyguardStatusArea.setRowPadding(mIsTypeClock ? mContext.getResources()
-                .getDimensionPixelSize(R.dimen.keyguard_status_area_typeclock_padding) : 0, 0, 0,
-                0);
     }
 
     public void refreshLockFont() {
