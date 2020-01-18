b599013c26bc387b17d804d098a82a6f53e212a9
Josh Chasky <jchasky@gmail.com>
2020-01-18 12:18:39 -0500

    base: Add Fira Sans font [1/2]
    Change-Id: I8c085b5a2d660e1c6831c0c635d3bed7aea6d324
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontBariolSourceOverlay \
 	FontCagliostroSourceOverlay \
 	FontCoolstorySourceOverlay \
+    FontFiraSansSourceOverlay \
 	FontLGSmartGothicSourceOverlay \
 	FontNotoSerifSourceOverlay \
 	FontRosemarySourceOverlay \
