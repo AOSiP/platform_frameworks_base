28b3e4427df5f9164f9509d36d2b506831f31823
maxwen <max.weninger@gmail.com>
2020-01-28 15:57:42 +0100

    SystemUI: trigger pulse light only for notifications
    store the doze trigger reason in a setting to check later
    if it has been triggered for a notification or any other reason
    
    Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4761,6 +4761,13 @@ public final class Settings {
         private static final Validator AMBIENT_NOTIFICATION_LIGHT_ACTIVATED_VALIDATOR =
                 BOOLEAN_VALIDATOR;
 
+         /**
+         * helper setting to contain the current pulse reason so we can check
+         * if it has been triggered from a notification or a gesture
+         * @hide
+         */
+        public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
+
         /**
          * Whether to show Brightness Icon On Brightness Slider
          * @hide
