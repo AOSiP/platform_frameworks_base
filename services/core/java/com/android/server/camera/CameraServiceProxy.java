9bbc5d3bfc330deb35436536ca57ba40522727c4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-02-06 08:22:58 +0300

    core: extended camera intents for camera state
    device side overlay also needed
    
    Change-Id: Ie1f3200e4b6eeee6962b14e2dd9ba051697e11b0

--- services/core/java/com/android/server/camera/CameraServiceProxy.java
+++ services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -78,6 +78,7 @@ public class CameraServiceProxy extends SystemService
     // Handler message codes
     private static final int MSG_SWITCH_USER = 1;
     private static final int MSG_CAMERA_CLOSED = 1001;
+    private static final int MSG_CAMERA_OPEN = 1002;
 
     private static final int RETRY_DELAY_TIME = 20; //ms
     private static final int RETRY_TIMES = 30;
@@ -105,7 +106,6 @@ public class CameraServiceProxy extends SystemService
     private static final String NFC_NOTIFICATION_PROP = "ro.camera.notify_nfc";
     private static final String NFC_SERVICE_BINDER_NAME = "nfc";
     private static final IBinder nfcInterfaceToken = new Binder();
-    private final boolean mAllowMediaUid;
 
     // Valid values for NFC_NOTIFICATION_PROP
     // Do not disable active NFC for any camera use
@@ -127,8 +127,11 @@ public class CameraServiceProxy extends SystemService
 
     private final @NfcNotifyState int mNotifyNfc;
     private boolean mLastNfcPollState = true;
+    private final boolean mAllowMediaUid;
+    private final boolean mSendCameraStatusIntent;
 
     private long mClosedEvent;
+    private long mOpenEvent;
 
     /**
      * Structure to track camera usage
@@ -223,16 +226,20 @@ public class CameraServiceProxy extends SystemService
 
             updateActivityCount(cameraId, newCameraState, facing, clientName, apiLevel);
 
-            if (facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
+            if (mSendCameraStatusIntent && facing == ICameraServiceProxy.CAMERA_FACING_FRONT) {
                 switch (newCameraState) {
                    case ICameraServiceProxy.CAMERA_STATE_OPEN : {
+                       mOpenEvent = SystemClock.elapsedRealtime();
                        if (SystemClock.elapsedRealtime() - mClosedEvent < CAMERA_EVENT_DELAY_TIME) {
                            mHandler.removeMessages(MSG_CAMERA_CLOSED);
                        }
-                       sendCameraStateIntent("1");
+                       mHandler.sendEmptyMessageDelayed(MSG_CAMERA_OPEN, CAMERA_EVENT_DELAY_TIME);
                    } break;
                    case ICameraServiceProxy.CAMERA_STATE_CLOSED : {
                        mClosedEvent = SystemClock.elapsedRealtime();
+                       if (SystemClock.elapsedRealtime() - mOpenEvent < CAMERA_EVENT_DELAY_TIME) {
+                           mHandler.removeMessages(MSG_CAMERA_OPEN);
+                       }
                        mHandler.sendEmptyMessageDelayed(MSG_CAMERA_CLOSED, CAMERA_EVENT_DELAY_TIME);
                    } break;
                 }
@@ -255,6 +262,8 @@ public class CameraServiceProxy extends SystemService
         if (DEBUG) Slog.v(TAG, "Notify NFC state is " + nfcNotifyToString(mNotifyNfc));
         mAllowMediaUid = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_allowMediaUidForCameraServiceProxy);
+        mSendCameraStatusIntent = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_sendCameraStatusIntent);
     }
 
     @Override
@@ -265,6 +274,9 @@ public class CameraServiceProxy extends SystemService
             } break;
             case MSG_CAMERA_CLOSED: {
                 sendCameraStateIntent("0");
+            } break;
+            case MSG_CAMERA_OPEN: {
+                sendCameraStateIntent("1");
             } break;
             default: {
                 Slog.e(TAG, "CameraServiceProxy error, invalid message: " + msg.what);
