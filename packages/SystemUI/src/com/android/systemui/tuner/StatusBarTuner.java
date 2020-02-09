75ea5c2c76da2144f8833858d4cab7090b11954b
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-02-09 00:06:46 -0500

    base: move 4g/lte switch out of tuner[1/2]
    Change-Id: I796a32bf9d677074d75b5f11109b2bbf9dc4f863

--- packages/SystemUI/src/com/android/systemui/tuner/StatusBarTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/StatusBarTuner.java
@@ -15,16 +15,10 @@
  */
 package com.android.systemui.tuner;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.provider.Settings;
 import android.os.Bundle;
-import android.os.UserHandle;
 import android.view.MenuItem;
 
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -32,24 +26,11 @@ import com.android.systemui.R;
 
 public class StatusBarTuner extends PreferenceFragment {
 
-    private static final String SHOW_FOURG = "show_fourg";
-
-    private SwitchPreference mShowFourG;
-
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
-        mShowFourG = (SwitchPreference) findPreference(SHOW_FOURG);
-        if (isWifiOnly()) {
-            getPreferenceScreen().removePreference(mShowFourG);
-        } else {
-            mShowFourG.setChecked(Settings.System.getIntForUser(getActivity().getContentResolver(),
-                Settings.System.SHOW_FOURG,
-                getActivity().getResources().getBoolean(R.bool.config_show4GForLTE) ? 1 : 0,
-                UserHandle.USER_CURRENT) == 1);
-        }
     }
 
     @Override
@@ -76,22 +57,5 @@ public class StatusBarTuner extends PreferenceFragment {
             return true;
         }
         return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mShowFourG) {
-            boolean checked = ((SwitchPreference)preference).isChecked();
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SHOW_FOURG, checked ? 1 : 0);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    private boolean isWifiOnly() {
-        ConnectivityManager cm = (ConnectivityManager)getActivity().getSystemService(
-                Context.CONNECTIVITY_SERVICE);
-        return (cm != null && cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
     }
 }
