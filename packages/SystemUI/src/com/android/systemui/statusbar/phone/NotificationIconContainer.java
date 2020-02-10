bce4d1a9e4de5b4cc0f91046a59b99979dd35d29
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-02-11 00:10:42 +0530

    SystemUI: Link config_maxVisibleNotificationIcons{,whenDark} to internal
    A ton of devices (read: almost all) have the overlay in SystemUI, link it to internal so that devices
    that want to overlay in NoCutoutOverlay can and devices that already have it in SystemUI have functionality.
    
    Change-Id: I6df4a0962bc24333f4298e6fe93348f0530ebe44
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconContainer.java
@@ -129,9 +129,9 @@ public class NotificationIconContainer extends AlphaOptimizedFrameLayout {
     }.setDuration(CONTENT_FADE_DURATION);
 
     private final int MAX_VISIBLE_ICONS_ON_LOCK =
-            getResources().getInteger(com.android.internal.R.integer.config_maxVisibleNotificationIconsWhenDark);
+            getResources().getInteger(R.integer.config_maxVisibleNotificationIconsWhenDark);
     public final int MAX_STATIC_ICONS =
-            getResources().getInteger(com.android.internal.R.integer.config_maxVisibleNotificationIcons);
+            getResources().getInteger(R.integer.config_maxVisibleNotificationIcons);
     private static final int MAX_DOTS = 1;
 
     private boolean mIsStaticLayout = true;
