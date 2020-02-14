4e7f6630382b24c8390749d9a0ffb7ee8883222d
maxwen <max.weninger@gmail.com>
2020-02-14 02:50:08 +0100

    SystemUI: day 0 notification light puilse fixes
    use correct default color resource
    stop animator when no longer needed
    
    Change-Id: I6d80245d281dc08a9cf366b7df8668bb977165ef

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -3429,7 +3429,7 @@ public class NotificationPanelView extends PanelView implements
                 if (activeNotif && pulseReasonNotification) {
                     // show the bars if we have to
                     if (pulseLights) {
-                        mPulseLightsView.animateNotification(true);
+                        mPulseLightsView.animateNotification();
                         mPulseLightsView.setVisibility(View.VISIBLE);
                     } else {
                         // bars can still be visible as leftover
@@ -3453,7 +3453,7 @@ public class NotificationPanelView extends PanelView implements
                     if (ambientLightsHideAod) {
                         showAodContent(false);
                     }
-                    mPulseLightsView.animateNotification(true);
+                    mPulseLightsView.animateNotification();
                     mPulseLightsView.setVisibility(View.VISIBLE);
                     mAmbientPulseLightRunning = true;
                     if (ambientLightsTimeout != 0) {
@@ -3675,6 +3675,7 @@ public class NotificationPanelView extends PanelView implements
             Log.d(TAG, "stopNotificationPulse mAmbientPulseLightRunning = " + mAmbientPulseLightRunning);
         }
         mPulseLightsView.setVisibility(View.GONE);
+        mPulseLightsView.stopAnimateNotification();
         Settings.System.putIntForUser(mContext.getContentResolver(),
                 Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0,
                 UserHandle.USER_CURRENT);
