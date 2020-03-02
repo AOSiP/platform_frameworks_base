6f386401d427f1c23c87b567aabd64a1d6002ad1
Alex <du.alexcruz@gmail.com>
2020-03-01 19:07:01 -0500

    Gestures: Add haptic feedback to left/right long swipe actions
    Change-Id: Ia28c0dbef7b408e423e802212f93b640f57e2842

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -40,6 +40,8 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -197,6 +199,8 @@ public class EdgeBackGestureHandler implements DisplayListener {
     private boolean mBlockNextEvent;
     private boolean mIsExtendedSwipe;
 
+    private final Vibrator mVibrator;
+
     public EdgeBackGestureHandler(Context context, OverviewProxyService overviewProxyService) {
         final Resources res = context.getResources();
         mContext = context;
@@ -225,6 +229,8 @@ public class EdgeBackGestureHandler implements DisplayListener {
         mAssistManager = Dependency.get(AssistManager.class);
         mHandler = new Handler();
         setLongSwipeOptions();
+
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public void updateCurrentUserResources(Resources res) {
@@ -622,6 +628,7 @@ public class EdgeBackGestureHandler implements DisplayListener {
             mBlockNextEvent = true;
             mEdgePanel.resetOnDown();
             triggerAction(mIsOnLeftEdge);
+            mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
         }
     }
 
