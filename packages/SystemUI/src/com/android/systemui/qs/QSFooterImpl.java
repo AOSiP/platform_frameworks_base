9089b836df514714b127b31283a64f8acc188bf1
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-03-08 10:38:05 -0400

    Revert "base: SystemUI: add tuner to hide qs drag handle"
    This reverts commit bd3ba692fa96c2595d21cd10c3f51be39850493e.
    
    Change-Id: I3d623e3570f6c7532253b50345928a2b6b71b286

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -66,7 +66,6 @@ import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserInfoController.OnUserInfoChangedListener;
 import com.android.systemui.tuner.TunerService;
-import com.android.systemui.tuner.TunerService.Tunable;
 
 import static android.content.Context.VIBRATOR_SERVICE;
 
@@ -74,10 +73,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 public class QSFooterImpl extends FrameLayout implements QSFooter,
-        OnClickListener, OnLongClickListener , OnUserInfoChangedListener, Tunable {
+        OnClickListener, OnLongClickListener , OnUserInfoChangedListener {
 
     private static final String TAG = "QSFooterImpl";
-    public static final String QS_SHOW_DRAG_HANDLE = "qs_show_drag_handle";
 
     private final ActivityStarter mActivityStarter;
     private final UserInfoController mUserInfoController;
@@ -240,24 +238,13 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-
-        final TunerService tunerService = Dependency.get(TunerService.class);
-        tunerService.addTunable(this, QS_SHOW_DRAG_HANDLE);
     }
 
     @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
-        Dependency.get(TunerService.class).removeTunable(this);
         setListening(false);
         super.onDetachedFromWindow();
-    }
-
-    @Override
-    public void onTuningChanged(String key, String newValue) {
-        if (QS_SHOW_DRAG_HANDLE.equals(key)) {
-            setHideDragHandle(newValue != null && Integer.parseInt(newValue) == 0);
-        }
     }
 
     @Override
@@ -386,9 +373,5 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
                     Mode.SRC_IN);
         }
         mMultiUserAvatar.setImageDrawable(picture);
-    }
-
-    private void setHideDragHandle(boolean hide) {
-        mDragHandle.setVisibility(hide ? View.GONE : View.VISIBLE);
     }
 }
