cc1c632955bc2c22c8c0506e8807f4e50fe655f1
El Dainosor <eldainosor@gmail.com>
2020-04-05 07:50:43 +0000

    Screenrec: Switch to low quality switch to a bitrate selector
    This is inspired on OOS, and allows the user to pick the video quality, at their own risk.
    Also, it supports Android Go, with a dedicated amount of options to pick on.
    
    Change-Id: Ida167685dce1a12b70f176fcdde4381d2239b795

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4677,10 +4677,10 @@ public final class Settings {
         public static final String SCREENRECORD_STOP_DOT = "screenrecord_stop_dot";
 
         /**
-         * Screenrecord: low quality
+         * Screenrecord: video bitrate
          * @hide
          */
-        public static final String SCREENRECORD_LOW_QUALITY = "screenrecord_low_quality";
+        public static final String SCREENRECORD_VIDEO_BITRATE = "screenrecord_video_bitrate";
 
         /** Whether to pulse ambient on new music tracks
          *
