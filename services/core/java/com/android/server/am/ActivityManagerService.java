c75a063626252ad05f4a0220f42a2281edf62531
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-08 21:35:58 +0300

    Make sensor block package list configurable [1/2]
    Based on https://github.com/crdroidandroid/android_frameworks_base/commit/2f0ff544cee1de199856958405e08ad074ee443e
    
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Change-Id: Ic972385bbb8bebe7113c20913ef52e8a20f56f66

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -231,6 +231,8 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Rect;
+import android.hardware.SensorManager;
+import android.hardware.SystemSensorManager;
 import android.hardware.display.DisplayManagerInternal;
 import android.location.LocationManager;
 import android.media.audiofx.AudioEffect;
@@ -1671,6 +1673,8 @@ public class ActivityManagerService extends IActivityManager.Stub
     private static String sTheRealBuildSerial = Build.UNKNOWN;
 
     private ParcelFileDescriptor[] mLifeMonitorFds;
+
+    private SystemSensorManager mSystemSensorManager;
 
     static final HostingRecord sNullHostingRecord = new HostingRecord(null);
 
@@ -7923,6 +7927,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         RescueParty.onSettingsProviderPublished(mContext);
 
         //mUsageStatsService.monitorPackages();
+
+        mSystemSensorManager = new SystemSensorManager(mContext, mHandler.getLooper());
     }
 
     void startPersistentApps(int matchFlags) {
@@ -16200,6 +16206,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                                         mServices.forceStopPackageLocked(ssp, userId);
                                         mAtmInternal.onPackageUninstalled(ssp);
                                         mBatteryStatsService.notePackageUninstalled(ssp);
+                                        if (mSystemSensorManager != null) {
+                                            mSystemSensorManager.notePackageUninstalled(ssp);
+                                        }
                                     }
                                 } else {
                                     if (killProcess) {
