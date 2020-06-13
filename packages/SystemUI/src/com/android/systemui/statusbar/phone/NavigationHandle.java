dfe987cbd95e73af2dab948bb7eff262e3df86b1
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-06-13 08:23:27 -0400

    Revert "base: wrap the size of the navigation handle"
    and bring inline with du edits
    
    This reverts commit fd354f218607967b4e2ae3eea4f86c87d5ead370.
    
    Change-Id: Iebc9f34e025edc1812618773dc61b13dcb989110

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationHandle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationHandle.java
@@ -16,10 +16,9 @@
 
 package com.android.systemui.statusbar.phone;
 
-import static android.provider.Settings.System.NAVIGATION_HANDLE_WIDTH;
-
 import android.animation.ArgbEvaluator;
 import android.annotation.ColorInt;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
@@ -41,7 +40,10 @@ public class NavigationHandle extends View implements ButtonInterface {
     private @ColorInt final int mDarkColor;
     private final int mRadius;
     private final int mBottom;
-    private final int mBaseWidth;
+    private int mWidth;
+
+    private Resources mRes;
+    private ContentResolver mResolver;
 
     public NavigationHandle(Context context) {
         this(context, null);
@@ -49,10 +51,10 @@ public class NavigationHandle extends View implements ButtonInterface {
 
     public NavigationHandle(Context context, AttributeSet attr) {
         super(context, attr);
-        final Resources res = context.getResources();
-        mRadius = res.getDimensionPixelSize(R.dimen.navigation_handle_radius);
-        mBottom = res.getDimensionPixelSize(R.dimen.navigation_handle_bottom);
-        mBaseWidth = res.getDimensionPixelSize(R.dimen.navigation_home_handle_width);
+        mRes = context.getResources();
+        mResolver = context.getContentResolver();
+        mRadius = mRes.getDimensionPixelSize(R.dimen.navigation_handle_radius);
+        mBottom = mRes.getDimensionPixelSize(R.dimen.navigation_handle_bottom);
 
         final int dualToneDarkTheme = Utils.getThemeAttr(context, R.attr.darkIconTheme);
         final int dualToneLightTheme = Utils.getThemeAttr(context, R.attr.lightIconTheme);
@@ -71,15 +73,14 @@ public class NavigationHandle extends View implements ButtonInterface {
         // Draw that bar
         int navHeight = getHeight();
         int height = mRadius * 2;
-        int width = getWidth();
+        mWidth = (int) getCustomWidth();
         int y = (navHeight - mBottom - height);
-        canvas.drawRoundRect(0, y, width, y + height, mRadius, mRadius, mPaint);
+        int padding = (int) getCustomPadding();
+        canvas.drawRoundRect(padding, y, mWidth + padding, y + height, mRadius, mRadius, mPaint);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(getCustomWidth() + getPaddingLeft() + getPaddingRight(),
-                getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec));
+    private double getCustomPadding() {
+        return (getWidth() / 2) - (mWidth / 2);
     }
 
     @Override
@@ -108,19 +109,17 @@ public class NavigationHandle extends View implements ButtonInterface {
     public void setDelayTouchFeedback(boolean shouldDelay) {
     }
 
-    private int getCustomWidth() {
-        /* 0: small (stock AOSP)
-           1: medium
-           2: long
-        */
-        int userSelection = Settings.System.getInt(getContext().getContentResolver(),
-                NAVIGATION_HANDLE_WIDTH, 0);
+    private double getCustomWidth() {
+        int baseWidth = mRes.getDimensionPixelSize(R.dimen.navigation_home_handle_width);
+
+        String WIDTH_SETTING = "navigation_handle_width";
+        int userSelection = Settings.System.getInt(mResolver, WIDTH_SETTING, 0);
         if (userSelection == 0) {
-            return mBaseWidth;
+            return baseWidth;
         } else if (userSelection == 1) {
-            return (int) (1.33 * mBaseWidth);
+            return 1.33 * baseWidth;
         } else {
-            return 2 * mBaseWidth;
+            return 2 * baseWidth;
         }
     }
 }
