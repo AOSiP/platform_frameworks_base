DirtyUnicorns/android_frameworks_base

Add kill app action to navbar actions
Change-Id: Ic865539cc731ade9458d0d30dc5688797cb6a59d

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6530,6 +6530,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 ActionUtils.toggleRingerModes(mContext);
                 break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                ActionUtils.killForegroundApp();
+                break;
         }
     }
 
@@ -6615,6 +6618,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 ActionUtils.toggleRingerModes(mContext);
+                break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                ActionUtils.killForegroundApp();
                 break;
         }
     }

AOSiP/platform_frameworks_base

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6731,6 +6731,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 aosipUtils.toggleRingerModes(mContext);
                 break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                aosipUtils.killForegroundApp();
+                break;
         }
     }
 
@@ -6816,6 +6819,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case NavbarUtilities.KEY_ACTION_RINGER_MODES:
                 aosipUtils.toggleRingerModes(mContext);
+                break;
+            case NavbarUtilities.KEY_ACTION_KILL_APP:
+                aosipUtils.killForegroundApp();
                 break;
         }
     }