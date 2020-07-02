4ce4b8f888f6d46b8d5320f29989f21f29ea0361
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-07-02 18:14:05 -0400

    Revert "Add new font overlays"
    This reverts commit 4d7f26a5e90c374d0b68e2fa19c16d98d932e711.
    
    Change-Id: I778e35cbc4bc78670587c5bc6a947ee5c13dec20

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -27,10 +27,7 @@ LOCAL_REQUIRED_MODULES := \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
 	DisplayCutoutEmulationTallOverlay \
-	FontArbutusSourceOverlay \
-	FontArvoLatoOverlay \
 	FontNotoSerifSourceOverlay \
-	FontRubikRubikOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
