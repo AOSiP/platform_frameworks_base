e53ff2858542265bc6f59c1bb76d429ef6ae1471
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-07-11 13:30:29 +0300

    base: Add Screen OFF FOD [1/2]
    Change-Id: Ic45cf7619951480714153a571f1f94fa5207ce65
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3168,6 +3168,15 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 3);
 
         /**
+          * Swipe to screenshot
+          * @hide
+          */
+         public static final String FOD_GESTURE = "fod_gesture";
+ 
+         /** @hide */
+         public static final Validator FOD_GESTURE_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * END_BUTTON_BEHAVIOR value for "go home".
          * @hide
          */
@@ -6053,6 +6062,7 @@ public final class Settings {
             VALIDATORS.put(VIBRATE_ON_DISCONNECT, VIBRATE_ON_DISCONNECT_VALIDATOR);
             VALIDATORS.put(VOLTE_ICON_STYLE, VOLTE_ICON_STYLE_VALIDATOR);
             VALIDATORS.put(VOLUME_BUTTON_MUSIC_CONTROL, VOLUME_BUTTON_MUSIC_CONTROL_VALIDATOR);
+            VALIDATORS.put(FOD_GESTURE, FOD_GESTURE_VALIDATOR);
         }
 
         /**
