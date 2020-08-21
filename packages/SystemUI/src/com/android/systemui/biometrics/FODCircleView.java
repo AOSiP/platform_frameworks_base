43b9d8ad506d6552003d085a033839cf77baa7ab
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-08-21 21:05:09 +0300

    FODCircleView: Enable hardware acceleration
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I3cf5a8cb4f78702675d920ae269588c51fb7fc56

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -179,7 +179,8 @@ public class FODCircleView extends ImageView {
         mParams.packageName = "android";
         mParams.type = WindowManager.LayoutParams.TYPE_DISPLAY_OVERLAY;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
