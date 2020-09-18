721827ba4ee664e73a4fe40d3eda74d5b4d1eee7
DennySPb <dennyspb@gmail.com>
2020-09-18 15:50:50 +0300

    SystemUI: adapt flashlight api to R
    Dependency.get replaced by constructor initializers
    
    Change-Id: I4463b85c7440c5239414ae63a1ec966fb79cda98

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -708,6 +708,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             PluginManager pluginManager,
             Optional<Divider> dividerOptional,
             LightsOutNotifController lightsOutNotifController,
+            FlashlightController flashlightController,
             StatusBarNotificationActivityStarter.Builder
                     statusBarNotificationActivityStarterBuilder,
             ShadeController shadeController,
@@ -769,6 +770,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mDeviceProvisionedController = deviceProvisionedController;
         mNavigationBarController = navigationBarController;
         mAssistManagerLazy = assistManagerLazy;
+        mFlashlightController = flashlightController;
         mConfigurationController = configurationController;
         mNotificationShadeWindowController = notificationShadeWindowController;
         mLockscreenLockIconController = lockscreenLockIconController;
@@ -1269,7 +1271,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
 
-        mFlashlightController = Dependency.get(FlashlightController.class);
     }
 
     @VisibleForTesting
