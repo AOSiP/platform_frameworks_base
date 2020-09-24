DirtyUnicorns/android_frameworks_base

Fonts: check vendor for fonts.xml
Change-Id: I6133fed38b4d1dfab6f194ab517765fc6b61000d

data/fonts/Android.mk
@@ -89,7 +89,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := fonts.xml
 LOCAL_MODULE_CLASS := ETC
-LOCAL_PREBUILT_MODULE_FILE := frameworks/base/data/fonts/fonts.xml
+LOCAL_PREBUILT_MODULE_FILE := vendor/themes/fonts/prebuilt/fonts.xml
 
 include $(BUILD_PREBUILT)
 

AOSiP/platform_frameworks_base

data/fonts/Android.mk
@@ -89,7 +89,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := fonts.xml
 LOCAL_MODULE_CLASS := ETC
-LOCAL_PREBUILT_MODULE_FILE := frameworks/base/data/fonts/fonts.xml
+LOCAL_PREBUILT_MODULE_FILE := vendor/overlay/fonts/prebuilt/fonts.xml
 
 include $(BUILD_PREBUILT)
 