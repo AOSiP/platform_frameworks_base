f5b60e5fd7ed3d66fce3b6ac8f55bc8009183d65
spezi77 <spezi7713@gmx.net>
2021-03-01 14:42:39 +0100

    Ambient music: Skip useless setVisibility calls.
    Trigger the setVisibility only when the doze state changes in order to
    skip useless setVisibility calls.
    
    Thanks very much @ezio84 for the idea to improve it!
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Idc0cfead16c1348d954f00ef94952ae3ea1e6191

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -48,6 +48,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
     private View mAmbientIndication;
     private boolean mDozing;
     private boolean mKeyguard;
+    private boolean mVisible;
     private StatusBar mStatusBar;
     private TextView mText;
     private Context mContext;
@@ -118,7 +119,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
         mInfoAvailable = false;
         mNpInfoAvailable = false;
         mText.setText(null);
-        mAmbientIndication.setVisibility(View.INVISIBLE);
+        setVisibility(false);
     }
 
     public void initializeView(StatusBar statusBar, Handler handler) {
@@ -166,23 +167,32 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
     }
 
     public void updateKeyguardState(boolean keyguard) {
-        if (keyguard && (mInfoAvailable || mNpInfoAvailable)) {
-            mText.setText(mInfoToSet);
-            mLastInfo = mInfoToSet;
-        } else {
-            mText.setText(null);
-            mAmbientIndication.setVisibility(View.INVISIBLE);
+        if (keyguard != mKeyguard) {
+            mKeyguard = keyguard;
+            if (keyguard && (mInfoAvailable || mNpInfoAvailable)) {
+                mText.setText(mInfoToSet);
+                mLastInfo = mInfoToSet;
+            } else {
+                mText.setText(null);
+            }
+            setVisibility(shouldShow());
         }
-        mKeyguard = keyguard;
     }
 
     public void updateDozingState(boolean dozing) {
         if (mDozing != dozing) {
             mDozing = dozing;
+            setVisibility(shouldShow());
         }
-        mAmbientIndication.setVisibility(shouldShow() ? View.VISIBLE : View.INVISIBLE);
-        if (hasInDisplayFingerprint() && shouldShow()) {
-            updatePosition();
+    }
+
+    private void setVisibility(boolean shouldShow) {
+        if (mVisible != shouldShow) {
+            mVisible = shouldShow;
+            mAmbientIndication.setVisibility(shouldShow ? View.VISIBLE : View.INVISIBLE);
+            if (hasInDisplayFingerprint() && shouldShow) {
+                updatePosition();
+            }
         }
     }
 
@@ -243,10 +253,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer implement
         }
         if (mInfoToSet != null) {
             mText.setText(mInfoToSet);
-            mAmbientIndication.setVisibility(shouldShow() ? View.VISIBLE : View.INVISIBLE);
-            if (hasInDisplayFingerprint() && shouldShow()) {
-                updatePosition();
-            }
+            setVisibility(shouldShow());
         } else {
             hideIndication();
         }
