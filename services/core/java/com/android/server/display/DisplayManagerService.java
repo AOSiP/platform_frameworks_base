3f96adceb65102b29f99883acc5bc412c6403c11
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-06-08 19:00:09 -0400

    base: remove leftovers from google r38 merge/revert mess
    Change-Id: I709040019a0da9e4765247fbbb23cfbc5ccbf3e1

--- services/core/java/com/android/server/display/DisplayManagerService.java
+++ services/core/java/com/android/server/display/DisplayManagerService.java
@@ -1484,14 +1484,6 @@ public final class DisplayManagerService extends SystemService {
         }
     }
 
-    void setDisplayModeDirectorLoggingEnabled(boolean enabled) {
-        synchronized (mSyncRoot) {
-            if (mDisplayModeDirector != null) {
-                mDisplayModeDirector.setLoggingEnabled(enabled);
-            }
-        }
-    }
-
     void setAmbientColorTemperatureOverride(float cct) {
         if (mDisplayPowerController != null) {
             synchronized (mSyncRoot) {
